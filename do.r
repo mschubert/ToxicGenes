3d94ec5234ee348f13cb9d78d478892942a3a19a
mschubert <mschu.dev@gmail.com>
2019-06-17 14:36:31 +0200

    plot; tissues

--- do.r
+++ do.r
@@ -1,4 +1,13 @@
+library(cowplot)
 io = import('io')
+
+plot_overview = function(data, label_top=20) {
+    data %>%
+        mutate(label = ifelse(rank(`LFC DMSO/ETP`) <= label_top, `GENE SYMBOL`, NA)) %>%
+        ggplot(aes(x=DMSO, y=`LFC DMSO/ETP`)) +
+            geom_point(alpha=0.5) +
+            ggrepel::geom_label_repel(aes(label=label))
+}
 
 expr = io$read_table("./data/ORF_DMSO_2019-02.txt", header=TRUE) %>%
     tidyr::gather("condition", "value", -(`Construct Barcode`:`BEST GENE MATCH`)) %>%
@@ -7,3 +16,8 @@ expr = io$read_table("./data/ORF_DMSO_2019-02.txt", header=TRUE) %>%
            cells = sub(" .*$", "", condition),
            condition = sub("^[A-Za-z0-9-]+ ", "", condition)) %>%
     tidyr::spread(condition, value)
+
+percell = expr %>%
+    group_by(cells) %>%
+    tidyr::nest() %>%
+    purrr::map(data, plot_overview)

a0f07bcbbf7d36579a5a2f62456277de5e5fa9c6
mschubert <mschu.dev@gmail.com>
2019-06-17 22:00:00 +0200

    custom loess

--- overview.r
+++ overview.r
@@ -3,16 +3,27 @@ io = import('io')
 
 tissues = io$read_table("tissues.txt", header=TRUE)
 
+loess_sd = function(fml, data) {
+    x = data[[as.character(fml[[3]])]]
+    y = data[[as.character(fml[[2]])]]
+    mod = msir::loess.sd(x, y, data=data)
+    df = data.frame(x = mod$x, y=mod$y, sd=mod$sd)
+    df = df[seq(1, nrow(df), length.out=100),]
+    list(geom_line(data=df, aes(x=x, y=y), color="red", size=1),
+         geom_line(data=df, aes(x=x, y=y+sd), color="red", linetype="dotted", size=1),
+         geom_line(data=df, aes(x=x, y=y-sd), color="red", linetype="dotted", size=1))
+}
+
 plot_overview = function(data, title, label_top=20) {
-    data %>%
-        mutate(label = ifelse(rank(`LFC DMSO/ETP`) <= label_top, `GENE SYMBOL`, NA)) %>%
-        ggplot(aes(x=DMSO, y=`LFC DMSO/ETP`)) +
-            geom_point(alpha=0.5) +
-            geom_smooth(color="red", se=FALSE) +
-            ggrepel::geom_label_repel(aes(label=label)) +
-            labs(title = title,
-                 x = "DMSO",
-                 y = "LFC DMSO/ETP")
+    data = data %>%
+        mutate(label = ifelse(rank(`LFC DMSO/ETP`) <= label_top, `GENE SYMBOL`, NA))
+    ggplot(data, aes(x=DMSO, y=`LFC DMSO/ETP`)) +
+        geom_point(alpha=0.5) +
+        loess_sd(`LFC DMSO/ETP` ~ DMSO, data=data) +
+        ggrepel::geom_label_repel(aes(label=label), size=2) +
+        labs(title = title,
+             x = "DMSO",
+             y = "LFC DMSO/ETP")
 }
 
 expr = io$read_table("./data/ORF_DMSO_2019-02.txt", header=TRUE) %>%

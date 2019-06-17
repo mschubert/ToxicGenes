2f584713d59089041491bae2cb09d3414548101a
mschubert <mschu.dev@gmail.com>
2019-06-17 22:50:13 +0200

    add stat_loess_sd (only mean so far)

--- overview.r
+++ overview.r
@@ -3,10 +3,8 @@ io = import('io')
 
 tissues = io$read_table("tissues.txt", header=TRUE)
 
-loess_sd = function(fml, data) {
-    x = data[[as.character(fml[[3]])]]
-    y = data[[as.character(fml[[2]])]]
-    mod = msir::loess.sd(x, y, data=data)
+loess_sd = function(x, y) {
+    mod = msir::loess.sd(x, y)
     df = data.frame(x = mod$x, y=mod$y, sd=mod$sd)
     df = df[seq(1, nrow(df), length.out=100),]
     list(geom_line(data=df, aes(x=x, y=y), color="red", size=1),
@@ -14,12 +12,31 @@ loess_sd = function(fml, data) {
          geom_line(data=df, aes(x=x, y=y-sd), color="red", linetype="dotted", size=1))
 }
 
+stat_loess_sd = function(mapping = NULL, data = NULL, geom = "line",
+                        position = "identity", na.rm = FALSE, show.legend = NA, 
+                        inherit.aes = TRUE, ...) {
+
+    statLoessSd = ggproto("statLoessSd", Stat,
+        compute_group = function(data, scales) {
+            mod = msir::loess.sd(data$x, data$y)
+            df = data.frame(x = mod$x, y=mod$y, sd=mod$sd)
+            df = df[seq(1, nrow(df), length.out=100),]
+        },
+        required_aes = c("x", "y")
+    )
+
+    layer(stat = statLoessSd, data = data, mapping = mapping, geom = geom, 
+          position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+          params = list(na.rm = na.rm, ...))
+}
+
 plot_overview = function(data, title, label_top=20) {
     data = data %>%
         mutate(label = ifelse(rank(`LFC DMSO/ETP`) <= label_top, `GENE SYMBOL`, NA))
     ggplot(data, aes(x=DMSO, y=`LFC DMSO/ETP`)) +
         geom_point(alpha=0.5) +
-        loess_sd(`LFC DMSO/ETP` ~ DMSO, data=data) +
+        loess_sd(data$DMSO, data$`LFC DMSO/ETP`) +
+#        stat_loess_sd(color="red", size=1) +
         ggrepel::geom_label_repel(aes(label=label), size=2) +
         labs(title = title,
              x = "DMSO",

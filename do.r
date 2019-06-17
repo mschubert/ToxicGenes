b275c3bcf5a9e7d74626d6e5fdef36a147f39b50
mschubert <mschu.dev@gmail.com>
2019-06-17 15:30:05 +0200

    save pdf

--- do.r
+++ do.r
@@ -1,12 +1,17 @@
 library(cowplot)
 io = import('io')
 
-plot_overview = function(data, label_top=20) {
+tissues = io$read_table("tissues.txt", header=TRUE)
+
+plot_overview = function(data, title, label_top=20) {
     data %>%
         mutate(label = ifelse(rank(`LFC DMSO/ETP`) <= label_top, `GENE SYMBOL`, NA)) %>%
         ggplot(aes(x=DMSO, y=`LFC DMSO/ETP`)) +
             geom_point(alpha=0.5) +
-            ggrepel::geom_label_repel(aes(label=label))
+            ggrepel::geom_label_repel(aes(label=label)) +
+            labs(title = title,
+                 x = "DMSO",
+                 y = "LFC DMSO/ETP")
 }
 
 expr = io$read_table("./data/ORF_DMSO_2019-02.txt", header=TRUE) %>%
@@ -14,10 +19,17 @@ expr = io$read_table("./data/ORF_DMSO_2019-02.txt", header=TRUE) %>%
     mutate(condition = sub("( ORF)?[_-]DMSO", " DMSO", condition),
            condition = sub("LFC$", "LFC DMSO/ETP", condition),
            cells = sub(" .*$", "", condition),
+           cells = sub("LnCAP", "LnCaP", cells, fixed=TRUE),
+           cells = sub("SKNEP1", "SK-NEP-1", cells, fixed=TRUE),
            condition = sub("^[A-Za-z0-9-]+ ", "", condition)) %>%
     tidyr::spread(condition, value)
 
 percell = expr %>%
     group_by(cells) %>%
     tidyr::nest() %>%
-    purrr::map(data, plot_overview)
+    mutate(plot = purrr::map2(data, cells, plot_overview))
+
+pdf("overview.pdf")
+for (p in percell$plot)
+    print(p)
+dev.off()

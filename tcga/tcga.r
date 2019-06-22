09c89ecd1c16472f52e24c6f57ed5bc883680aa3
mschubert <mschu.dev@gmail.com>
2019-06-22 18:05:53 +0200

    model compartments + delta

--- tcga/tcga.r
+++ tcga/tcga.r
@@ -4,31 +4,35 @@ plt = import('plot')
 tcga = import('data/tcga')
 idmap = import('process/idmap')
 
-do_fit = function(gene, emat, copies, covar=NA) {
+do_fit = function(gene, emat, copies, purity, covar=NA) {
     on.exit(message("Error: ", gene))
     df = data.frame(expr = emat[gene,] / mean(emat[gene,], na.rm=TRUE) - 1,
                     erank = rank(emat[gene,]),
                     copies = copies[gene,],
+                    purity = purity,
+                    other = 1 - purity,
+                    delta = (copies[gene,] - 2) / purity * purity,
                     covar = covar) %>%
+    select(-covar) %>%
         na.omit()
 
     has_covar = length(unique(na.omit(covar))) > 1
     if (has_covar) {
-        f1 = erank ~ covar + copies
-        f2 = expr ~ covar + copies
+        f1 = erank ~ 0 + covar + copies + other + delta
+        f2 = expr ~ 0 + covar + copies + other + delta
     } else {
-        f1 = erank ~ copies
-        f2 = expr ~ copies
+        f1 = erank ~ 0 + copies + other + delta
+        f2 = expr ~ 0 + copies + other + delta
     }
 
     prank = lm(f1, data=df) %>%
         broom::tidy() %>%
-        filter(term == "copies") %>%
+        filter(term == "delta") %>%
         pull(p.value)
 
     mod = MASS::rlm(f2, data=df, maxit=100) %>%
         broom::tidy() %>%
-        filter(term == "copies") %>%
+        filter(term == "delta") %>%
         select(-term) %>%
         mutate(n_aneup = sum(abs(df$copies-2) > 0.2),
                p.value = prank)
@@ -37,7 +41,7 @@ do_fit = function(gene, emat, copies, covar=NA) {
     mod
 }
 
-all_fits = function(emat, copies, tissues=NA) {
+all_fits = function(emat, copies, purity, tissues=NA) {
     ffuns = list(
         amp = function(x) { x[x < 1.8] = NA; x },
         del = function(x) { x[x > 2.2] = NA; x },
@@ -47,9 +51,9 @@ all_fits = function(emat, copies, tissues=NA) {
 
     do_ffun = function(ffun) {
         copies2 = ffun(copies)
-        fit_gene = function(g) do_fit(g, emat, copies2, tissues)
+        fit_gene = function(g) do_fit(g, emat, copies2, purity$estimate, tissues)
         tibble(gene = rownames(emat)) %>%
-            mutate(res = parallel::mclapply(gene, fit_gene)) %>%
+            mutate(res = lapply(gene, fit_gene)) %>%
             tidyr::unnest() %>%
             mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
             arrange(adj.p, p.value)
@@ -81,14 +85,14 @@ sys$run({
     rownames(reads) = idmap$gene(rownames(reads), to="hgnc_symbol")
     reads = reads[rowMeans(reads) >= 10,]
 
-    if (args$tissue == "SKCM") #TODO:
-        reads = reads[rownames(reads) != "TRGC1",]
+#    if (args$tissue == "SKCM") #TODO:
+#        reads = reads[rownames(reads) != "TRGC1",]
 
     copies = tcga$cna_genes(args$tissue)
     rownames(copies) = idmap$gene(rownames(copies), to="hgnc_symbol")
     narray::intersect(purity$Sample, reads, copies, along=2)
 #    copies[] = copies / narray::rrep(purity$estimate, nrow(copies)) #TODO: how to adjust this?
-    copies[] = t(t(copies) / purity$estimate)
+#    copies[] = t(t(copies) / purity$estimate)
     copies = na.omit(copies)
     narray::intersect(reads, copies, along=1)
 
@@ -96,13 +100,13 @@ sys$run({
     rownames(cdata) = colnames(reads)
 
     emat = DESeq2::DESeqDataSetFromMatrix(reads, cdata, ~1) %>%
-        DESeq2::estimateSizeFactors(normMatrix=copies) %>%
+        DESeq2::estimateSizeFactors(normMatrix=copies) %>% # total ploidy to scale lib size
         DESeq2::counts(normalized=TRUE)
 
 #    if (args$tissue != "pan")
 #        dset$idx$tcga_code[dset$idx$tcga_code != args$tissue] = NA
 
-    fits = all_fits(emat, copies, purity$estimate)
+    fits = all_fits(emat, copies, purity)
     plots = lapply(fits, do_plot)
 
     pdf(args$plotfile)

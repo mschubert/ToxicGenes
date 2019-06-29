8c543e1b7f6b960a11b1eefd811214211b46e050
mschubert <mschu.dev@gmail.com>
2019-06-29 17:25:15 +0200

    filter gene vs background distribution

--- orf/fits.r
+++ orf/fits.r
@@ -3,25 +3,32 @@ library(cowplot)
 plt = import('plot')
 sys = import('sys')
 
-do_fit = function(expr, ffun) {
+do_fit = function(expr, fml) {
+    ffun = function(g) {
+        expr %>%
+            mutate(gene = `Construct IDs` == g) %>%
+            lm(fml, data=.) %>%
+            broom::tidy()
+    }
+
     res = expr %>%
-        group_by(`GENE SYMBOL`, `Construct IDs`) %>%
-        tidyr::nest() %>%
-        mutate(result = purrr::map(data, function(d) broom::tidy(lm(ffun, data=d)))) %>%
-        select(-data) %>%
+        select(`GENE SYMBOL`, `Construct IDs`) %>%
+        distinct() %>%
+        mutate(result = purrr::map(`Construct IDs`, ffun)) %>%
         tidyr::unnest() %>%
-        filter(term == "(Intercept)") %>%
+        filter(term == "geneTRUE") %>%
         select(-term) %>%
         mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
         arrange(adj.p, p.value)
 }
 
 do_plot = function(res) {
+    res$label = res$`GENE SYMBOL`
     if (all(res$estimate[rank(res$p.value) < 10] > 0))
         res$label[res$estimate > 0] = NA
 
     res %>%
-        mutate(label=`GENE SYMBOL`, size=5) %>%
+        mutate(size=5) %>%
         plt$color$p_effect(pvalue="adj.p", effect="estimate", thresh=0.1, dir=-1) %>%
         plt$volcano(label_top=30, repel=TRUE, x_label_bias=4)
 }
@@ -37,14 +44,14 @@ sys$run({
         mutate(`LFC DMSO/ETP` = `LFC DMSO/ETP` + runif(nrow(.)) * 0.01)
 
     pan = list(
-        pan = do_fit(expr, `LFC DMSO/ETP` ~ 1),
-        pancov = do_fit(expr, `LFC DMSO/ETP` ~ tissue + 1)
+        pan = do_fit(expr, `LFC DMSO/ETP` ~ gene),
+        pancov = do_fit(expr, `LFC DMSO/ETP` ~ tissue + gene)
     )
 
     tissue = sapply(c("NB", "OV", "BRCA", "SKCM", "MB"),
                     function(t) expr %>%
                         filter(tissue %in% t) %>%
-                        do_fit(`LFC DMSO/ETP` ~ 1),
+                        do_fit(`LFC DMSO/ETP` ~ gene),
                     simplify=FALSE)
 
     result = c(pan, tissue)
@@ -55,4 +62,4 @@ sys$run({
     dev.off()
 
     writexl::write_xlsx(result, args$outfile)
-}
+})

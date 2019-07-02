1c014fd616d5e54fd617923bd230106c9875a754
mschubert <mschu.dev@gmail.com>
2019-07-02 14:15:09 +0200

    split plot into pos/neg

--- ccle/plot.r
+++ ccle/plot.r
@@ -1,19 +1,31 @@
 library(dplyr)
+library(patchwork)
 plt = import('plot')
 sys = import('sys')
 
-do_plot = function(data) {
+do_plot = function(data, title) {
     ylab = "Adjusted p-value (FDR)"
     if (sum(data$adj.p < 1e-300) > 5) {
         data$adj.p = 10^-abs(data$statistic)
         ylab = "Pseudo p-value (values too close to zero)"
     }
 
-    p = data %>%
+    p1 = data %>%
+        filter(estimate < 0) %>%
         plt$color$p_effect(pvalue="adj.p", effect="estimate") %>%
-        plt$volcano(base.size=0.2, label_top=50, repel=TRUE,
-                    text.size=2.5, x_label_bias=5, pos_label_bias=0.15) +
-        ylab(ylab)
+        plt$volcano(base.size=0.2, text.size=2.5, label_top=30, repel=TRUE) +
+        labs(title = title,
+             subtitle = "compensated",
+             y = ylab)
+
+    p2 = data %>%
+        filter(estimate > 0) %>%
+        plt$color$p_effect(pvalue="adj.p", effect="estimate") %>%
+        plt$volcano(base.size=0.2, text.size=2.5, label_top=30, repel=TRUE) +
+        labs(subtitle="hyper-deregulated") +
+        theme(axis.title.y = element_blank())
+
+    p1 + p2 + plot_layout(nrow=1)
 }
 
 sys$run({
@@ -29,8 +41,8 @@ sys$run({
     else
         fits = lapply(fits, function(f) mutate(f, label=set))
 
-    pdf(args$plotfile)
+    pdf(args$plotfile, 12, 8)
     for (i in seq_along(fits))
-        print(do_plot(fits[[i]]) + ggtitle(names(fits)[i]))
+        print(do_plot(fits[[i]], names(fits)[i]))
     dev.off()
 })

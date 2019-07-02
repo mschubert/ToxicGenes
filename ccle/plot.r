596d7a150e52b7da941ee53a1e2baf4fca40d185
mschubert <mschu.dev@gmail.com>
2019-07-02 16:28:14 +0200

    handle missing assocs, stat>300

--- ccle/plot.r
+++ ccle/plot.r
@@ -6,24 +6,32 @@ sys = import('sys')
 do_plot = function(data, title) {
     ylab = "Adjusted p-value (FDR)"
     if (sum(data$adj.p < 1e-300) > 5) {
-        data$adj.p = 10^-abs(data$statistic)
+        data$adj.p = 10^-abs(pmin(data$statistic, 300))
         ylab = "Pseudo p-value (values too close to zero)"
     }
 
-    p1 = data %>%
-        filter(estimate < 0) %>%
-        plt$color$p_effect(pvalue="adj.p", effect="estimate") %>%
-        plt$volcano(base.size=0.2, text.size=2.5, label_top=30, repel=TRUE) +
-        labs(title = title,
-             subtitle = "compensated",
-             y = ylab)
-
-    p2 = data %>%
-        filter(estimate > 0) %>%
-        plt$color$p_effect(pvalue="adj.p", effect="estimate") %>%
-        plt$volcano(base.size=0.2, text.size=2.5, label_top=30, repel=TRUE) +
-        labs(subtitle="hyper-deregulated") +
-        theme(axis.title.y = element_blank())
+    p1 = tryCatch({ 
+        p = data %>%
+            filter(estimate < 0) %>%
+            plt$color$p_effect(pvalue="adj.p", effect="estimate") %>%
+            plt$volcano(base.size=0.2, text.size=2.5, label_top=30, repel=TRUE) +
+            labs(title = title,
+                 subtitle = "compensated",
+                 y = ylab)
+        ggplot_build(p)
+        p
+    }, error = function(e) plt$text(conditionMessage(e)))
+        
+    p2 = tryCatch({
+        p = data %>%
+            filter(estimate > 0) %>%
+            plt$color$p_effect(pvalue="adj.p", effect="estimate") %>%
+            plt$volcano(base.size=0.2, text.size=2.5, label_top=30, repel=TRUE) +
+            labs(subtitle="hyper-deregulated") +
+            theme(axis.title.y = element_blank())
+        ggplot_build(p)
+        p
+    }, error = function(e) plt$text(conditionMessage(e)))
 
     p1 + p2 + plot_layout(nrow=1)
 }
@@ -42,7 +50,9 @@ sys$run({
         fits = lapply(fits, function(f) mutate(f, label=set))
 
     pdf(args$plotfile, 12, 8)
-    for (i in seq_along(fits))
+    for (i in seq_along(fits)) {
+        message(names(fits)[i])
         print(do_plot(fits[[i]], names(fits)[i]))
+    }
     dev.off()
 })

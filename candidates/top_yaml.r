aa5a8dd719f682be26370ec6f566111d2fc8a6d8
mschubert <mschu.dev@gmail.com>
2019-07-26 22:46:49 +0200

    include amp+del+all for top choice

--- candidates/top_yaml.r
+++ candidates/top_yaml.r
@@ -2,24 +2,24 @@ io = import('io')
 sys = import('sys')
 
 args = sys$cmd$parse(
-    opt('i', 'infile', 'dset', 'merge_genes.rds'),
+    opt('i', 'infile', 'dset', 'merge/pan/genes.rds'),
     opt('s', 'select', 'genes|gene set', 'genes'),
-    opt('n', 'num', 'number of top hits', '10'),
-    opt('o', 'outfile', 'yaml', 'top_genes.yaml'))
+    opt('n', 'num', 'number of top hits', '12'),
+    opt('o', 'outfile', 'yaml', 'pan/top_genes.yaml'))
 
 dset = readRDS(args$infile)
 
 top = dset %>%
+    filter(adj %in% c("none", "puradj")) %>%
     group_by(dset, fit, adj) %>%
     mutate(score = (1-adj.p) * rank(-statistic) / length(statistic)) %>%
-    group_by(name, dset, fit) %>% # summarize adjustment (tcga: naive, pur, puradj)
-    summarize(score = mean(score, na.rm=TRUE)) %>%
-    group_by(name, dset) %>% # summarize by fit (rlm, rank)
+    group_by(name, dset, fit) %>% # select most significant cna (amp, del, all)
+    top_n(1, -score) %>%
+    group_by(name, dset) %>% # mean by fit (rlm, rank)
     summarize(score = mean(score, na.rm=TRUE)) %>%
     group_by(name) %>% # summarize by dset (orf, ccle, tcga)
-    filter(all(score > 0)) %>%
     summarize(n_dset = length(name),
-              score = sum(score, na.rm=TRUE) / n_dset^0) %>% # ^0 is sum (CDKN1A), ^1 is mean (LOCxxx)
+              score = sum(score, na.rm=TRUE)) %>% # root prioritizes consistency across dsets
     arrange(-score)
 
 obj = list(head(top$name, as.integer(args$num)))

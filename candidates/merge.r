56d10227437fb35b579b854d412592a1f46bd23c
mschubert <mschu.dev@gmail.com>
2019-10-28 17:41:00 +0100

    plot high-scoring overlap matrix

--- candidates/merge.r
+++ candidates/merge.r
@@ -7,8 +7,7 @@ args = sys$cmd$parse(
     opt('s', 'sets', 'genes|gene set', 'genes'),
     opt('t', 'tissue', 'pan|TCGA identifier', 'pan'),
     opt('o', 'outfile', 'rds', 'merge/pan/genes.rds'),
-    opt('p', 'plotfile', 'pdf', 'merge/pan/genes.pdf'),
-    opt('x', 'xlsx', 'xlsx', 'merge/pan/genes.xlsx'))
+    opt('p', 'plotfile', 'pdf', 'merge/pan/genes.pdf'))
 
 orf = readxl::read_xlsx(sprintf("../orf/%s/%s.xlsx", args$tissue, args$sets)) %>%
     mutate(adj = "none", fit = "lm", cna = "oe")
@@ -43,19 +42,35 @@ dset = list(orf=orf, ccle=ccle, tcga=tcga) %>%
 
 saveRDS(dset, file=args$outfile)
 
-wide = dset %>%
-    filter(adj %in% c("none", "puradj")) %>%
-    transmute(name=name, subs=paste("fdr", dset, cna, sep="_"), sfdr=sign(statistic) * adj.p) %>%
-    distinct(name, subs, .keep_all=TRUE) %>% # 19 dups pan gene
-    tidyr::spread(subs, sfdr) %>%
-    select(name, fdr_orf_oe, everything())
-score = order(rowSums(1 - abs(wide[,-1]), na.rm=TRUE))
-wide = wide[rev(score),]
-writexl::write_xlsx(wide, args$xlsx)
-
-venn = . %>% filter(estimate < 0, adj.p < 0.1) %>% select(name, dset) %>% distinct() %>% unstack()
+venn = function(title, df, comp=-0.5, fdr=0.01, r2=0.05) {
+    df2 = df %>%
+        filter(estimate < comp, adj.p < fdr, dset=="orf" | rsq > r2) %>%
+        select(name, dset) %>% distinct()
+    df3 = unstack(df2)
+    df3 = df3[sapply(df3, length) > 0]
+    p1 = plt$venn(df3) +
+        ggtitle(sprintf("%s >= %i%% comp, %i%% FDR, %i%% R^2", title,
+                        round(-comp*100), round(fdr*100), round(r2*100)))
+    df4 = df2 %>%
+        group_by(name) %>%
+        mutate(n = n()) %>%
+        ungroup() %>%
+        arrange(-n) %>%
+        filter(n >= 2) %>%
+        mutate(name = factor(name, levels=rev(sort(unique(name)))))
+    p2 = ggplot(df4, aes(x=factor(dset, levels=c("orf", "ccle", "tcga")), y=name)) +
+        geom_tile(color="white", fill="black", size=0.5) +
+        coord_fixed() +
+        theme(axis.title.x = element_blank(),
+              axis.title.y = element_blank(),
+              axis.text.x = element_text(size=6, angle=90, hjust=1, vjust=0.5),
+              axis.text.y = element_text(size=6))
+    p1 + p2 + plot_layout(widths=c(5,1))
+}
 pdf(args$plotfile)
-plt$venn(venn(dset %>% filter(cna %in% c("oe", "all")))) + ggtitle("ALL compensated, 10% FDR")
-plt$venn(venn(dset %>% filter(cna %in% c("oe", "amp")))) + ggtitle("AMP compensated, 10% FDR")
-plt$venn(venn(dset %>% filter(cna %in% c("oe", "del")))) + ggtitle("DEL compensated, 10% FDR")
+venn("amp+del", dset %>% filter(cna %in% c("oe", "all")))
+venn("amp+del", dset %>% filter(cna %in% c("oe", "all")), r2=0.2)
+venn("amp", dset %>% filter(cna %in% c("oe", "amp")))
+venn("amp", dset %>% filter(cna %in% c("oe", "amp")), r2=0.2)
+venn("del", dset %>% filter(cna %in% c("del")))
 dev.off()

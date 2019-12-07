f613e01a75333f1c84c665717cc4a36c15c10455
mschubert <mschu.dev@gmail.com>
2019-12-07 21:17:59 +0100

    excl rlm (not effect size possible), plot error instead of failing

--- merge_cutoff/plot_venn.r
+++ merge_cutoff/plot_venn.r
@@ -12,9 +12,9 @@ venn = function(title, df, comp=-0.5, fdr=c(0.05,1e-3), r2=0.02) {
         select(name, dset) %>% distinct()
     df3 = unstack(df2)
     df3 = df3[sapply(df3, length) > 0]
-    p1 = plt$venn(df3) +
-        ggtitle(sprintf("%s >= %i%% comp, %i/%.1g%% FDR, %i%% R^2", title,
-                        round(-comp*100), round(fdr[1]*100), round(fdr[2]*100), round(r2*100)))
+    p1 = plt$try(plt$venn(df3)) +
+        ggtitle(sprintf("%s >= %i%% comp, %i/%.2g%% FDR, %i%% R^2", title,
+                        round(-comp*100), round(fdr[1]*100), fdr[2]*100, round(r2*100)))
     df4 = df2 %>%
         group_by(name) %>%
         mutate(n = n()) %>%
@@ -22,13 +22,15 @@ venn = function(title, df, comp=-0.5, fdr=c(0.05,1e-3), r2=0.02) {
         arrange(-n) %>%
         filter(n >= 2) %>%
         mutate(name = factor(name, levels=rev(sort(unique(name)))))
+    tsize = min(6, round(450 / length(levels(df4$name))))
     p2 = ggplot(df4, aes(x=factor(dset, levels=c("orf", "ccle", "tcga")), y=name)) +
-        geom_tile(color="white", fill="black", size=0.5) +
+        geom_tile(aes(fill=factor(n)), color="white", size=0.5, show.legend=FALSE) +
         coord_fixed() +
+        scale_fill_manual(values=c("black", "red")) +
         theme(axis.title.x = element_blank(),
               axis.title.y = element_blank(),
-              axis.text.x = element_text(size=6, angle=90, hjust=1, vjust=0.5),
-              axis.text.y = element_text(size=6))
+              axis.text.x = element_text(size=tsize, angle=90, hjust=1, vjust=0.5),
+              axis.text.y = element_text(size=tsize))
     p1 + p2 + plot_layout(widths=c(5,1))
 }
 
@@ -43,9 +45,9 @@ sys$run({
 
     plots = list(
         venn("amp+del", dset %>% filter(cna %in% c("oe", "all"))),
-        venn("amp+del", dset %>% filter(cna %in% c("oe", "all")), r2=0.1),
+        venn("amp+del", dset %>% filter(cna %in% c("oe", "all")), fdr=c(0.05,0.01), r2=0.1),
         venn("amp", dset %>% filter(cna %in% c("oe", "amp"))),
-        venn("amp", dset %>% filter(cna %in% c("oe", "amp")), r2=0.1),
+        venn("amp", dset %>% filter(cna %in% c("oe", "amp")), fdr=c(0.05,0.01), r2=0.1),
         venn("del", dset %>% filter(cna %in% c("del")))
     )
     pdf(args$plotfile)

23e7d17254ed83c0b84184dc25d76c7bf1d4e026
mschubert <mschu.dev@gmail.com>
2019-12-10 13:20:17 +0100

    overview stubs

--- merge_rank/rank_overview.r
+++ merge_rank/rank_overview.r
@@ -3,6 +3,6 @@ sys = import('sys')
 
 args = sys$cmd$parse(
     opt('d', 'dset', 'pan merge', '../merge/pan.rds'),
-    opt('p', 'plotfile', 'pdf', 'cor_overview.pdf'))
+    opt('p', 'plotfile', 'pdf', 'rank_overview.pdf'))
 
 dset = readRDS(args$dset)

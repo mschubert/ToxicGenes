6d58eca7b8351f3cdd4d8269a8ac04b507693833
mschubert <mschu.dev@gmail.com>
2019-12-26 17:49:49 +0100

    fix candidate plots to work w/ new structure

---
+++ candidates/yaml_top.r
@@ -0,0 +1,25 @@
+library(dplyr)
+sys = import('sys')
+
+args = sys$cmd$parse(
+    opt('i', 'infile', 'xlsx', '../merge_rank/rank_top/pan_rlm3.xlsx'),
+    opt('n', 'num', 'integer', '12'),
+    opt('o', 'outfile', 'yaml', 'pan/top-genes.yaml'))
+
+n = as.integer(args$num)
+
+sheets = readxl::excel_sheets(args$infile)
+top = sapply(sheets, simplify=FALSE, function(s) {
+    ranks = readxl::read_excel(args$infile)
+})
+
+res = list(
+    genes = list(
+        top12 = top$all$name[seq_len(n)],
+        top24 = top$all$name[seq_len(2*n)[-seq_len(n)]],
+        amp = top$amp$name[seq_len(n)],
+        del = top$del$name[seq_len(n)]
+    ),
+    methods = c("lm", "rlm", "rlm3")
+)
+yaml::write_yaml(res, file=args$outfile)
--- merge_rank/rank_top.r
+++ merge_rank/rank_top.r
@@ -1,27 +1,35 @@
 io = import('io')
 sys = import('sys')
 
+score = function(dset) {
+    score = dset %>%
+        filter(adj %in% c("none", "puradj"), #TODO: keep "none" for TCGA too???
+               fit %in% c("lm", args$fit)) %>%
+        mutate(rsq = ifelse(dset == "orf", 0.5, rsq), #TODO: better way? (value determines ORF weight)
+               estimate = ifelse(dset == "orf", 2^estimate - 1, estimate),
+               estimate = pmax(estimate, -1)) %>%
+        mutate(score = (1-adj.p) * (rank(-statistic) / length(statistic)) *
+                       pmax(0,rsq) * (-estimate))
+
+    score %>%
+        group_by(name, dset) %>% # mean by fit (rlm, rank)
+        summarize(score = mean(score, na.rm=TRUE)) %>%
+        group_by(name) %>% # summarize by dset (orf, ccle, tcga)
+        summarize(n_dset = length(name),
+                  score = sum(score^0.5, na.rm=TRUE)) %>% # root prioritizes consistency across dsets
+        arrange(-score) %>%
+        mutate(rank = 1:nrow(.))
+}
+
 args = sys$cmd$parse(
     opt('i', 'infile', 'dset', '../merge/pan.rds'),
     opt('f', 'fit', 'character', 'rlm3'),
     opt('o', 'outfile', 'xlsx', 'pan.xlsx'))
 
-dset = readRDS(args$infile) %>%
-    filter(adj %in% c("none", "puradj"), #TODO: keep "none" for TCGA too???
-           fit %in% c("lm", args$fit)) %>%
-    mutate(rsq = ifelse(dset == "orf", 0.5, rsq), #TODO: better way? (value determines ORF weight)
-           estimate = ifelse(dset == "orf", 2^estimate - 1, estimate),
-           estimate = pmax(estimate, -1)) %>%
-    mutate(score = (1-adj.p) * (rank(-statistic) / length(statistic)) *
-                   pmax(0,rsq) * (-estimate))
-
-top = dset %>%
-    group_by(name, dset) %>% # mean by fit (rlm, rank)
-    summarize(score = mean(score, na.rm=TRUE)) %>%
-    group_by(name) %>% # summarize by dset (orf, ccle, tcga)
-    summarize(n_dset = length(name),
-              score = sum(score^0.5, na.rm=TRUE)) %>% # root prioritizes consistency across dsets
-    arrange(-score) %>%
-    mutate(rank = 1:nrow(.))
-
+dset = readRDS(args$infile)
+top = list(
+    all = score(dset),
+    amp = score(dset %>% filter(cna %in% c("oe", "amp"))),
+    del = score(dset %>% filter(cna == "del"))
+)
 writexl::write_xlsx(top, args$outfile)

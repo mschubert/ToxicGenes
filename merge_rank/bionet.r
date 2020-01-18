58d159160e2ad1948c6d15b6886b82480fb9d188
mschubert <mschu.dev@gmail.com>
2020-01-18 21:48:00 +0100

    add briscut amp/del as fill

--- merge_rank/bionet.r
+++ merge_rank/bionet.r
@@ -32,7 +32,7 @@ plot_net = function(net, node_aes, ...) {
     p = ggraph(net) +
         geom_node_point(node_aes, ...) +
         geom_node_text(aes(label = name), size=2, repel=TRUE) +
-        viridis::scale_fill_viridis(option="magma", direction=-1) +
+        scale_color_gradient2(low="blue", high="red", mid="black") +
         theme_void()
     if (igraph::gsize(net) > 0)
         p = p + geom_edge_link(alpha=0.2)
@@ -54,19 +54,23 @@ plot_col = function(net, var) {
         mutate(name = factor(name, levels=rev(name)))
 
     ggplot(measures, aes_string(x="name", y=var)) +
-        geom_col() +
-        coord_flip()
+        geom_col(aes(fill=log10_ks_p)) +
+        scale_fill_gradient2(low="blue", high="red", mid="black") +
+        coord_flip() +
+        guides(fill=FALSE)
 }
 
 sys$run({
     args = sys$cmd$parse(
+        opt('t', 'tissue', 'pan|TCGA identifier', 'pan'),
         opt('f', 'fit', 'fit type id', 'rlm3'),
+        opt('b', 'briscut', 'txt file', '../data/all_BrISCUT_results.txt'),
         opt('r', 'ranks', 'xlsx', 'rank_top/pan_rlm3.xlsx'),
         opt('p', 'plotfile', 'pdf', 'bionet/pan/rlm3.pdf'))
 
-    if (grepl("BRCA", args$ranks)) {
+    if (args$tissue == "BRCA") {
         fdr = 0.23
-    } else if (grepl("pan|BRCA|OV|SKCM", args$ranks)) {
+    } else if (args$tissue %in% c("pan", "SKCM", "OV")) {
         fdr = 0.25
     } else {
         fdr = 0.3
@@ -78,18 +82,27 @@ sys$run({
     }
     top = sapply(readxl::excel_sheets(args$ranks), read_stats, simplify=FALSE)
 
+    cnv = setNames(c(1, -1), c("amp", "del"))
+    genomic = readr::read_tsv(args$briscut) %>%
+        transmute(cohort=type, name=Gene, log10_ks_p=pmin(log10_ks_p, 10) * cnv[direction])
+    if (args$tissue == "pan") {
+        genomic = genomic %>% group_by(name) %>% summarize(log10_ks_p = mean(log10_ks_p))
+    } else {
+        genomic = genomic %>% filter(cohort == args$tissue)
+    }
+
     net = OmnipathR::import_AllInteractions() %>%
         OmnipathR::interaction_graph() %>%
-        as_tbl_graph(directed=FALSE) %>%
-        activate(edges) %>%
-        select(-dip_url, -sources, -references) # ggraph issue #214
+        as_tbl_graph(directed=FALSE) %E>%
+        select(-dip_url, -sources, -references) %N>% # ggraph issue #214
+        left_join(genomic)
 
     res = lapply(top, function(t) bionet(net, t, thresh=fdr, var="pseudo_p"))
 
     pdf(args$plotfile, 8, 7)
     for (i in seq_along(res)) {
         p1 = plot_net(res[[i]] %N>% mutate(degree = centrality_degree()),
-                      aes(size=score, alpha=degree))
+                      aes(size=score, alpha=degree, color=log10_ks_p))
         p2 = plot_col(res[[i]], "score")
         p3 = plot_col(res[[i]], "degree")
         print(p1 + ggtitle(names(res)[i]) + (p2 / p3) + plot_layout(widths=c(8,1)))

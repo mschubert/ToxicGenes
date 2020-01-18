2c208160d4745c3a9324dba89084193db9a168d8
mschubert <mschu.dev@gmail.com>
2020-01-18 21:03:38 +0100

    add node degree as alpha

--- merge_rank/bionet.r
+++ merge_rank/bionet.r
@@ -1,6 +1,7 @@
 library(BioNet)
 library(ggraph)
 library(tidygraph)
+library(patchwork)
 io = import('io')
 sys = import('sys')
 idmap = import('process/idmap')
@@ -29,13 +30,32 @@ bionet = function(g, assocs, thresh=0.05, var="adj.p") {
 plot_net = function(net, node_aes, ...) {
     set.seed(121979) # same layout if same nodes
     p = ggraph(net) +
-        geom_node_point(node_aes, alpha=0.7, ...) +
+        geom_node_point(node_aes, ...) +
         geom_node_text(aes(label = name), size=2, repel=TRUE) +
         viridis::scale_fill_viridis(option="magma", direction=-1) +
         theme_void()
     if (igraph::gsize(net) > 0)
         p = p + geom_edge_link(alpha=0.2)
     p
+}
+
+#' Plot the highest degree nodes of network
+#'
+#' @param net  ggraph-compatible network object
+#' @return  ggplot2 object
+plot_col = function(net, var) {
+    vs = rlang::sym(var)
+    measures = net %N>%
+        mutate(degree = centrality_degree()) %>%
+        arrange(- !! vs) %>%
+        as_tibble() %>%
+        head(20) %>%
+        filter(!! vs > 0) %>%
+        mutate(name = factor(name, levels=rev(name)))
+
+    ggplot(measures, aes_string(x="name", y=var)) +
+        geom_col() +
+        coord_flip()
 }
 
 sys$run({
@@ -48,8 +68,9 @@ sys$run({
         fdr = 0.23
     } else if (grepl("pan|BRCA|OV|SKCM", args$ranks)) {
         fdr = 0.25
-    } else
+    } else {
         fdr = 0.3
+    }
 
     read_stats = function(sheet, ranks=args$ranks) {
         tab = readxl::read_xlsx(ranks, sheet=sheet) %>%
@@ -59,14 +80,19 @@ sys$run({
 
     net = OmnipathR::import_AllInteractions() %>%
         OmnipathR::interaction_graph() %>%
-        as_tbl_graph() %>%
+        as_tbl_graph(directed=FALSE) %>%
         activate(edges) %>%
         select(-dip_url, -sources, -references) # ggraph issue #214
 
     res = lapply(top, function(t) bionet(net, t, thresh=fdr, var="pseudo_p"))
 
-    pdf(args$plotfile)
-    for (i in seq_along(res))
-        print(plot_net(res[[i]], aes(size=score)) + ggtitle(names(res)[i]))
+    pdf(args$plotfile, 8, 7)
+    for (i in seq_along(res)) {
+        p1 = plot_net(res[[i]] %N>% mutate(degree = centrality_degree()),
+                      aes(size=score, alpha=degree))
+        p2 = plot_col(res[[i]], "score")
+        p3 = plot_col(res[[i]], "degree")
+        print(p1 + ggtitle(names(res)[i]) + (p2 / p3) + plot_layout(widths=c(8,1)))
+    }
     dev.off()
 })

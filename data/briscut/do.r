ea39b2ef8e938e0a79d2dda896f7b7bd59a22a2a
mschubert <mschu.dev@gmail.com>
2020-06-08 18:13:25 +0200

    get gene name, fdr

--- data/briscut/do.r
+++ data/briscut/do.r
@@ -16,14 +16,12 @@ max_one = function(genes) {
             pull(statistic)
     }
 
-    stat_obs = tcga %>% filter(name %in% genes) %>%
+    obs = tcga %>% filter(name %in% genes) %>%
         mutate(rnk = rank(-abs(statistic))) %>%
-        filter(rnk == 1) %>%
-        pull(statistic)
-    stat_exp = replicate(100, max_ng(length(genes)))
-    z = (stat_obs - mean(stat_exp)) / sd(stat_exp)
-    #list(z=z, p.value=2*pnorm(abs(z), lower.tail=FALSE))
-    2*pnorm(abs(z), lower.tail=FALSE)
+        filter(rnk == 1)
+    stat_exp = replicate(1000, max_ng(length(genes)))
+    z = (obs$statistic - mean(stat_exp)) / sd(stat_exp)
+    data.frame(Gene=obs$name, z=z, p.value=2*pnorm(abs(z), lower.tail=FALSE))
 }
 
 tcga = readxl::read_xlsx("../../tcga/pan/rlm3_pur.xlsx", sheet="amp")
@@ -34,7 +32,7 @@ brc = readr::read_tsv("all_BrISCUT_results_50under_200424.txt") %>%
     filter(Gene %in% tcga$name) %>%
     nest_by(type, Chr, Peak.Start, Peak.End, negpos) %>%
     filter(type == "PANCAN") %>%
-    mutate(maxdevp = purrr::map_dbl(data, max_one)) %>%
-    arrange(maxdevp)
-
+    mutate(maxdevp = purrr::map(data, max_one)) %>%
+    tidyr::unnest("maxdevp") %>%
+    mutate(adj.p = p.adjust(p.value, method="fdr"))
 

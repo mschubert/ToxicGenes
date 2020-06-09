89316d51e3808e8b9d67efb38ba9e4be8b3c4924
mschubert <mschu.dev@gmail.com>
2020-06-09 14:38:28 +0200

    add avg vs max, directed vs undir

--- data/briscut/do.r
+++ data/briscut/do.r
@@ -1,13 +1,43 @@
 library(dplyr)
+sys = import('sys')
 
-avg_one = function(df) {
-    stat = tcga %>% filter(name %in% df$Gene) %>% pull(statistic)
-    tt = t.test(stat) %>%
-        broom::tidy() %>%
-        select(estimate, p.value)
+avg_undirected = function(genes, .ignored) {
+    avg_ng = function(n) tcga %>% sample_n(n) %>% pull(statistic) %>% mean()
+
+    obs = tcga %>% filter(name %in% genes)
+    stat_exp = replicate(1000, avg_ng(length(genes)))
+    z = (mean(obs$statistic) - mean(stat_exp)) / sd(stat_exp)
+    data.frame(Gene=list(obs$name), z=z, p.value=2*pnorm(abs(z), lower.tail=FALSE))
 }
 
-max_one = function(genes, negpos) {
+avg_directed = function(genes, negpos) {
+    avg_ng = function(n) tcga %>% sample_n(n) %>% pull(statistic) %>% mean()
+
+    signs = setNames(c(-1,1),c("n","p"))
+    obs = tcga %>% filter(name %in% genes) %>% pull(statistic) %>% mean()
+    stat_exp = replicate(1000, avg_ng(length(genes)))
+    z = (mean(obs$statistic) - mean(stat_exp)) / sd(stat_exp)
+    data.frame(Gene=list(obs$name), z=z, p.value=2*pnorm(abs(z), lower.tail=FALSE))
+}
+
+max_undirected = function(genes, .ignored) {
+    max_ng = function(n) {
+        tcga %>%
+            sample_n(n) %>%
+            mutate(rnk = rank(-abs(statistic))) %>%
+            filter(rnk == 1) %>%
+            pull(statistic)
+    }
+
+    obs = tcga %>% filter(name %in% genes) %>%
+        mutate(rnk = rank(-abs(statistic))) %>%
+        filter(rnk == 1)
+    stat_exp = replicate(1000, max_ng(length(genes)))
+    z = (obs$statistic - mean(stat_exp)) / sd(stat_exp)
+    data.frame(Gene=obs$name, z=z, p.value=2*pnorm(abs(z), lower.tail=FALSE))
+}
+
+max_directed = function(genes, negpos) {
     max_ng = function(n) {
         tcga %>%
             sample_n(n) %>%
@@ -25,16 +55,25 @@ max_one = function(genes, negpos) {
     data.frame(Gene=obs$name, z=z, p.value=2*pnorm(abs(z), lower.tail=FALSE))
 }
 
-tcga = readxl::read_xlsx("../../tcga/pan/rlm3_pur.xlsx", sheet="amp")
+run = function(brc, fun) {
+    brc %>%
+        mutate(res = purrr::map2(data, negpos, fun)) %>%
+        tidyr::unnest("res") %>%
+        mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
+        arrange(adj.p)
+}
 
-brc = readr::read_tsv("all_BrISCUT_results_50under_200424.txt") %>%
-    select(-X1) %>%
-    select(type, Chr, Peak.Start, Peak.End, negpos, Gene) %>%
-    filter(Gene %in% tcga$name) %>%
-    nest_by(type, Chr, Peak.Start, Peak.End, negpos) %>%
-    filter(type == "PANCAN") %>%
-    mutate(maxdevp = purrr::map2(data, negpos, max_one)) %>%
-    tidyr::unnest("maxdevp") %>%
-    mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
-    arrange(adj.p)
+sys$run({
+    tcga = readxl::read_xlsx("../../tcga/pan/rlm3_pur.xlsx", sheet="amp")
 
+    brc = readr::read_tsv("all_BrISCUT_results_50under_200424.txt") %>%
+        select(-X1) %>%
+        select(type, Chr, Peak.Start, Peak.End, negpos, Gene) %>%
+        filter(Gene %in% tcga$name) %>%
+        nest_by(type, Chr, Peak.Start, Peak.End, negpos) %>%
+        ungroup() %>%
+        filter(type == "PANCAN")
+
+    ud = run(brc, max_undirected)
+    di = run(brc, max_directed)
+})

42df2b83ca3768d52370766934ae55fc2175ab3a
mschubert <mschu.dev@gmail.com>
2020-06-09 13:49:58 +0200

    include directed

--- data/briscut/do.r
+++ data/briscut/do.r
@@ -7,17 +7,18 @@ avg_one = function(df) {
         select(estimate, p.value)
 }
 
-max_one = function(genes) {
+max_one = function(genes, negpos) {
     max_ng = function(n) {
         tcga %>%
             sample_n(n) %>%
-            mutate(rnk = rank(-abs(statistic))) %>%
+            mutate(rnk = rank(-signs[negpos]*(statistic))) %>%
             filter(rnk == 1) %>%
             pull(statistic)
     }
 
+    signs = setNames(c(-1,1),c("n","p"))
     obs = tcga %>% filter(name %in% genes) %>%
-        mutate(rnk = rank(-abs(statistic))) %>%
+        mutate(rnk = rank(-signs[negpos]*(statistic))) %>%
         filter(rnk == 1)
     stat_exp = replicate(1000, max_ng(length(genes)))
     z = (obs$statistic - mean(stat_exp)) / sd(stat_exp)
@@ -32,7 +33,8 @@ brc = readr::read_tsv("all_BrISCUT_results_50under_200424.txt") %>%
     filter(Gene %in% tcga$name) %>%
     nest_by(type, Chr, Peak.Start, Peak.End, negpos) %>%
     filter(type == "PANCAN") %>%
-    mutate(maxdevp = purrr::map(data, max_one)) %>%
+    mutate(maxdevp = purrr::map2(data, negpos, max_one)) %>%
     tidyr::unnest("maxdevp") %>%
-    mutate(adj.p = p.adjust(p.value, method="fdr"))
+    mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
+    arrange(adj.p)
 

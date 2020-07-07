7923d1750adf88dd718382851805afae72eda4a4
mschubert <mschu.dev@gmail.com>
2020-07-07 12:04:57 +0200

    nicer racs labels

--- candidates/genomic_context.r
+++ candidates/genomic_context.r
@@ -39,36 +39,40 @@ plot_gene_track = function(gene="CDKN1A", et=0.15, len=1e8, bins=1000, hl=len/10
         mutate(external_gene_name = ifelse(end < center_pos-hl/2 | start > center_pos+hl/2,
                                            NA, external_gene_name))
 
+    arws = c(Amplification="▲", Deletion="▼")
+    ys = c(Amplification=max(dset$num)/2, Deletion=min(dset$num)/2)
     cracs = data_frame(seqnames = pos$seqnames,
                        start=max(0, center_pos - len/2)) %>%
         mutate(end = start + len) %>%
         GenomicRanges::makeGRangesFromDataFrame() %>%
         join_overlap_inner_within(racs %>% select(racs, cna), .) %>%
         as.data.frame() %>%
-        as_tibble()
+        as_tibble() %>%
+        mutate(label = paste(racs, arws[cna]), y = ys[cna])
 
     cols = c(gain="tomato", amp="firebrick", loss="lightblue", del="blue")
     p0 = ggplot(dset, aes(x=start)) +
-        geom_rect(data=cracs, aes(xmin=start, xmax=end), ymin=-Inf, ymax=Inf, fill="gold", alpha=0.15) +
+        geom_rect(data=cracs, aes(xmin=start, xmax=end), ymin=-Inf, ymax=Inf, fill="gold", alpha=0.1) +
         geom_vline(xintercept=c(center_pos-hl/2, center_pos+hl/2), linetype="dotted") +
         geom_ribbon(aes(ymax=num, group=type, fill=type), ymin=0, alpha=0.2) +
         geom_line(aes(y=num, color=type)) +
         scale_fill_manual(values=cols, guide=FALSE) +
         scale_color_manual(values=cols, guide=FALSE) +
-        geom_text(data=cracs, aes(x=(start+end)/2, label=paste(racs, cna, sep="\n")), y=max(dset$num)/2, size=2, hjust=0.5) +
         theme_classic() +
         geom_segment(data=genes, aes(xend=end), y=0, yend=0, size=2, alpha=0.5) +
         theme(axis.title.x = element_blank())
 
     p1 = p0 +
-        ggrepel::geom_label_repel(data=genes, size=2, max.iter=1e5, segment.alpha=0.3,
+        ggrepel::geom_text_repel(data=cracs, aes(x=(start+end)/2, y=y, label=label),
+            size=2, hjust=0.5, direction="x") +
+        ggrepel::geom_label_repel(data=genes, size=2, max.iter=1e5, segment.alpha=0.5,
             aes(x=0.5*(start+end), y=0, label=driver), min.segment.length=0,
             label.size=NA, fill="#ffffff80", segment.color="white") +
         ggtitle(sprintf("%s chr%i:%.0f-%.0f Mb", gene, pos$seqnames, min(dset$start)/1e6, max(dset$end)/1e6))
 
     p2 = p0 +
         coord_cartesian(xlim=c(center_pos-hl/2, center_pos+hl/2)) +
-        ggrepel::geom_label_repel(data=genes, size=2, max.iter=1e5, segment.alpha=0.3,
+        ggrepel::geom_label_repel(data=genes, size=2, max.iter=1e5, segment.alpha=0.5,
             aes(x=0.5*(start+end), y=0, label=external_gene_name),
             min.segment.length=0, label.size=NA, fill="#ffffff80", segment.color="white") +
         ggtitle(sprintf("%.1f-%.1f Mb", (center_pos-hl/2)/1e6, (center_pos+hl/2)/1e6))
@@ -118,7 +122,7 @@ racs2 = rtracklayer::liftOver(racs, hg19_to_hg38) %>%
 seqlevelsStyle(racs2) = "Ensembl"
 racs = racs2
 
-pdf(args$plotfile, 10, 4)
+cairo_pdf(args$plotfile, 10, 4, onefile=TRUE)
 for (g in c("EGFR", "ERBB2", "MYC", "CDKN1A", "RBM14", "RBM12", "HES1",
             "SNRPA", "NSF", "BANP", "H3F3C", "IRF2", "ZBTB14"))
     print(plot_gene_track(g))

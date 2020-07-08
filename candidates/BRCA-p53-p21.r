cdd24d5eed081c75a4a7038ee3c0c9408886b78b
mschubert <mschu.dev@gmail.com>
2020-07-08 11:45:35 +0200

    color by meth (whole gene, so not ideal)

--- candidates/BRCA-p53-p21.r
+++ candidates/BRCA-p53-p21.r
@@ -14,7 +14,7 @@ mut = tcga$mutations() %>%
 snp = mut %>% filter(Var == "SNP") %>% pull(Sample) %>% unique()
 del = mut %>% filter(Var == "DEL") %>% pull(Sample) %>% unique()
 
-td = util$load_tcga("BRCA", top="CDKN1A") %>%
+td = util$load_tcga("BRCA", top="CDKN1A") %>% #FIXME: is expr libsize corrected?
     select(-p53_mut) %>%
     mutate(p53_mut = case_when(
         sample %in% snp ~ "p53_snp",
@@ -31,7 +31,13 @@ mean_eup = td %>% filter(cancer_copies < 2+0.15) %>%
 pdf("BRCA-p53-p53.pdf", 10, 4)
 ggplot(td, aes(x=cancer_copies, y=expr)) +
     geom_abline(data=mean_eup, aes(slope=mean_eup/2, intercept=0), linetype="dashed", color="red") +
-    geom_point(alpha=0.2) +
+    geom_point(aes(color=meth_eup_scaled), size=2, alpha=0.5) +
+    geom_smooth(method="lm") +
+    scale_color_distiller(palette="RdBu") +
+    facet_grid(gene ~ p53_mut)
+
+ggplot(td, aes(x=cancer_copies, y=meth)) +
+    geom_point(size=2) +
     geom_smooth(method="lm") +
     facet_grid(gene ~ p53_mut)
 dev.off()

d33634ff8dc89c79947c566461499d0b8cf6c76e
mschubert <mschu.dev@gmail.com>
2020-07-22 13:38:41 +0200

    add BRCA subtypes

--- candidates/BRCA-p53-p21.r
+++ candidates/BRCA-p53-p21.r
@@ -58,13 +58,19 @@ mut = tcga$mutations() %>%
 snp = mut %>% filter(Var == "SNP") %>% pull(Sample) %>% unique()
 del = mut %>% filter(Var == "DEL") %>% pull(Sample) %>% unique()
 
-td = util$load_tcga("BRCA", top="CDKN1A") %>% #FIXME: is expr libsize corrected?
+brca_subtypes = readr::read_tsv("../../prmt5/BRCA.547.PAM50.SigClust.Subtypes.txt") %>%
+    transmute(sample = substr(Sample, 1, 16),
+              PAM50 = ifelse(PAM50 %in% c("Basal", "Her2"), PAM50, "LumAB+Normal"))
+
+td = util$load_tcga("BRCA", top="CDKN1A") %>%
     select(-p53_mut) %>%
     mutate(p53_mut = case_when(
         sample %in% snp ~ "p53_snp",
         sample %in% del ~ "p53_del",
         TRUE ~ "p53_wt")) %>%
-    filter(cancer_copies >= 2-0.15)
+    filter(cancer_copies >= 2-0.15) %>%
+    as_tibble() %>%
+    inner_join(brca_subtypes)
 table(td$p53_mut)
 #rlm3$do_fit()
 
@@ -76,26 +82,26 @@ mean_eup = td %>% filter(cancer_copies < 2+0.15) %>%
     group_by(gene, p53_mut) %>%
     summarize(mean_eup = mean(expr))
 
-pdf("BRCA-p53-p53.pdf", 10, 4)
+pdf("BRCA-p53-p21.pdf", 10, 8)
 ggplot(td, aes(x=cancer_copies, y=expr)) +
     geom_abline(data=mean_eup, aes(slope=mean_eup/2, intercept=0), linetype="dashed", color="red") +
     geom_point(aes(color=meth_eup_scaled), size=2, alpha=0.5) +
-    geom_smooth(method="lm") +
+    geom_smooth(method="lm", se=FALSE, color="red") +
     scale_color_distiller(palette="RdBu") +
-    facet_grid(gene ~ p53_mut)
+    facet_grid(PAM50 ~ p53_mut)
 
 ggplot(td, aes(x=cancer_copies, y=meth)) +
     geom_point(aes(alpha=purity), size=2) +
-    geom_smooth(method="lm", formula=y ~ x) +
-    facet_grid(gene ~ p53_mut) +
+    geom_smooth(method="lm", se=FALSE, color="blue") +
+    facet_grid(PAM50 ~ p53_mut) +
     ggtitle("methylation")
 
 td2 = cbind(td, cpg)
 for (cp in colnames(cpg)) {
     p = ggplot(td2, aes_string(x="cancer_copies", y=cp)) +
         geom_point(aes(alpha=purity), size=2) +
-        geom_smooth(method="lm") +
-        facet_grid(gene ~ p53_mut) +
+        geom_smooth(method="lm", se=FALSE, color="blue") +
+        facet_grid(PAM50 ~ p53_mut) +
         ggtitle(cp)
     print(p)
 }

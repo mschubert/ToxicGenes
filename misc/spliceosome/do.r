e8a40f4220e7bc82ed639293a623fbdbe760cb74
mschubertv <mschu.dev@gmail.com>
2020-10-28 10:28:00 +0000

    add plot w/o facet

--- misc/spliceosome/do.r
+++ misc/spliceosome/do.r
@@ -8,6 +8,7 @@ tcga = import('data/tcga')
 splice = readr::read_tsv("spliceosome.txt") %>%
     select(ensembl_gene_id = `Ensembl gene ID`,
            hgnc_symbol = `Approved symbol`,
+           chr = Chromosome,
            subunit = `Group name`) %>%
     distinct()
 
@@ -24,17 +25,30 @@ clust_x = tibble(Sample = colnames(cancer_copies),
 clust_y = tibble(ensembl_gene_id = rownames(cancer_copies),
                  umap_y = rank(uwot::umap(cancer_copies, n_components=1)[,1]))
 
-dset = reshape2::melt(cancer_copies) %>%
+# load compensation scores
+comp = readRDS()
+
+dset = reshape2::melt(cancer_copies, value.name="cancer_copies") %>%
     inner_join(clust_x) %>%
     inner_join(clust_y) %>%
     inner_join(splice) %>%
-#    plt$cluster(value ~ sid + Sample) %>%
-    mutate(value = pmin(value, 4))
+#    plt$cluster(cancer_copies ~ sid + Sample) %>%
+    mutate(cancer_copies = pmin(cancer_copies, 4),
+           hgnc_chr = sprintf("%s -- %s", hgnc_symbol, chr))
 
 # umap plot for different samples & genes
 pdf("do.pdf", 20, 12)
 ggplot(dset, aes(x=forcats::fct_reorder(Sample, umap_x),
-                 y=forcats::fct_reorder(ensembl_gene_id, umap_y), fill=value)) +
+                 y=forcats::fct_reorder(hgnc_chr, umap_y), fill=cancer_copies)) +
+    geom_tile() +
+    scale_fill_distiller(palette="Spectral") +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_text(size=5),
+          strip.text.y.left = element_text(angle = 0)) +
+    ggtitle("copy numbers")
+
+ggplot(dset, aes(x=forcats::fct_reorder(Sample, umap_x),
+                 y=forcats::fct_reorder(ensembl_gene_id, umap_y), fill=cancer_copies)) +
     geom_tile() +
     scale_fill_distiller(palette="Spectral") +
     facet_grid(subunit ~ ., space="free", scales="free", switch="y") +

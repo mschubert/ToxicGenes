d70767ab773f8c68ff00d3d4a9af0d3782c771d5
mschubert <mschu.dev@gmail.com>
2021-04-19 13:59:25 +0200

    add H838 RBM14 DE

---
+++ data/H838_RBM14/dset.r
@@ -0,0 +1,46 @@
+library(dplyr)
+library(magrittr)
+library(DESeq2)
+sys = import('sys')
+plt = import('plot')
+gset = import('genesets')
+
+args = sys$cmd$parse(
+    opt('c', 'config', 'yaml', '../../config.yaml'),
+#    opt('i', 'infile', 'txt', './210331_VR8768_viper/STAR/H838_luc_1/H838_luc_1.counts.tab'),
+    opt('o', 'outfile', 'rds', 'dset.rds'),
+    opt('p', 'plotfile', 'pdf', 'dset.pdf')
+)
+
+cfg = yaml::read_yaml(args$config)
+
+smps = c("H838_luc_1", "H838_RBM14_1", "H838_RBM14_2")
+reads = sprintf("./210331_VR8768_viper/STAR/%s/%s.counts.tab", smps, smps) %>%
+    lapply(readr::read_tsv, skip=4, col_names=FALSE) %>%
+    lapply(. %$% setNames(X2, X1)) %>%
+    setNames(smps) %>%
+    narray::stack(along=2)
+reads = reads[rowSums(reads) != 0,]
+
+meta = data.frame(condition = factor(c("luc", "RBM14", "RBM14"), levels=c("luc", "RBM14")))
+eset = DESeq2::DESeqDataSetFromMatrix(reads, meta, ~condition)
+res = DESeq2::DESeq(eset) %>%
+    DESeq2::results(name = "condition_RBM14_vs_luc") %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("gene_name") %>%
+    as_tibble() %>%
+    arrange(padj, pvalue)
+
+res$size = 1 # fixme @ volcano util
+
+#fixme: gset$get_human better error on invalid set name
+sets = sapply(cfg$genesets, function(s) readRDS(paste0("../genesets/", s, ".rds")), simplify=FALSE)
+scores = lapply(sets, gset$test, genes=res)
+
+pdf(args$plotfile, 10, 8)
+print(plt$volcano(res) + ggtitle("genes"))
+for (i in seq_along(scores))
+    print(plt$volcano(scores[[i]]) + ggtitle(names(scores)[i]))
+dev.off()
+
+saveRDS(eset, file=args$outfile)

af9412a0c26d669de140eaa10d97c4afa116bb1e
mschubertv <mschu.dev@gmail.com>
2021-05-07 10:58:48 +0000

    add args

--- cor_tcga_ccle/dset-cor.r
+++ cor_tcga_ccle/dset-cor.r
@@ -1,12 +1,25 @@
 library(dplyr)
 library(ggplot2)
+sys = import('sys')
 
-orf1 = readxl::read_xlsx("orf/fits_naive.xlsx") %>% dplyr::rename(gene=`GENE SYMBOL`)
-orf2 = readxl::read_xlsx("orf/fits_corrected.xlsx") %>% dplyr::rename(gene=`GENE SYMBOL`)
-ccle = readRDS("ccle/pan/stan-nb.rds")
-tcga1 = readRDS("tcga/pan/stan-nb_naive.rds")
-tcga2 = readRDS("tcga/pan/stan-nb_pur.rds")
-#tcga3 = readRDS("tcga/pan/stan-nb_puradj.rds")
+args = sys$cmd$parse(
+    opt('o', 'orf_naive', 'xlsx', '../orf/fits_naive.xlsx'),
+    opt('z', 'orf_corr', 'xlsx', '../orf/fits_corrected.xlsx'),
+    opt('c', 'ccle', 'xlsx', '../ccle/pan/rlm3.xlsx'),
+    opt('t', 'tcga_naive', 'xlsx', '../tcga/pan/rlm3_naive.xlsx'),
+    opt('p', 'tcga_pur', 'xlsx', '../tcga/pan/rlm3_pur.xlsx'),
+    opt('l', 'plotfile', 'pdf', 'pan.pdf')
+)
+
+orf1 = readxl::read_xlsx(args$orf_naive) %>% dplyr::rename(gene=`GENE SYMBOL`)
+orf2 = readxl::read_xlsx(args$orf_corr) %>% dplyr::rename(gene=`GENE SYMBOL`)
+ccle = readxl::read_xlsx(args$ccle, "amp") %>% dplyr::rename(gene=name) %>%
+    filter(adj.p < 1) %>% mutate(estimate = pmax(-2, pmin(estimate, 2.5)))
+tcga1 = readxl::read_xlsx(args$tcga_naive, "amp") %>% dplyr::rename(gene=name) %>%
+    filter(adj.p < 1) %>% mutate(estimate = pmax(-2, pmin(estimate, 2.5)))
+tcga2 = readxl::read_xlsx(args$tcga_pur, "amp") %>% dplyr::rename(gene=name) %>%
+    filter(adj.p < 1) %>% mutate(estimate = pmax(-2, pmin(estimate, 2.5)))
+#tcga3 = readxl::read_xlsx("tcga/pan/rlm3_puradj.xlsx", "amp")
 
 hl = c("RBM12", "RBM14", "SNRPA", "HNRNPL", "CDKN1A", "H3F3C", "DAZAP1", "BANP", "HHEX",
        "CEBPE", "HNRNPL", "HNRNPA2B1", "KLF2", "KLF4", "IRF2", "SOX15", "ELK3", "ADORA2A",
@@ -17,7 +30,7 @@ hl2 = grep("^(TR[ABG]V|IG[KL]V)", tcga1$gene, value=T)
 # tcga purity
 both = inner_join(tcga1 %>% transmute(gene=gene, tcga_naive=estimate),
            tcga2 %>% transmute(gene=gene, tcga_pur=sign(estimate)*pmin(abs(estimate), 5)))
-ggplot(both, aes(x=tcga_naive, y=tcga_pur)) +
+p1 = ggplot(both, aes(x=tcga_naive, y=tcga_pur)) +
     geom_point(alpha=0.1) +
     geom_point(data=both %>% filter(gene %in% hl), color="red") +
     geom_point(data=both %>% filter(gene %in% hl2), color="blue") +
@@ -29,7 +42,7 @@ both = ccle %>% transmute(gene=gene, ccle=estimate) %>%
     left_join(tcga1 %>% transmute(gene=gene, tcga_naive=estimate, tcga_aneup=n_aneup)) %>%
     left_join(tcga2 %>% transmute(gene=gene, tcga_pur=estimate))
 m0 = broom::glance(lm(ccle ~ tcga_naive, data=both)); m0
-ggplot(both, aes(x=ccle, y=tcga_naive)) +
+p2 = ggplot(both, aes(x=ccle, y=tcga_naive)) +
     geom_point(aes(size=pmin(500, tcga_aneup)), alpha=0.1) +
     geom_point(data=both %>% filter(gene %in% hl), aes(size=pmin(500, tcga_aneup)), color="red") +
     scale_size_area() +
@@ -37,7 +50,7 @@ ggplot(both, aes(x=ccle, y=tcga_naive)) +
     labs(title = "stan-nb", subtitle=sprintf("R^2=%.2g", m0$r.squared))
 
 m0 = broom::glance(lm(ccle ~ tcga_pur, data=both)); m0
-ggplot(both, aes(x=ccle, y=tcga_pur)) +
+p3 = ggplot(both, aes(x=ccle, y=tcga_pur)) +
     geom_point(aes(size=pmin(500, tcga_aneup)), alpha=0.1) +
     geom_point(data=both %>% filter(gene %in% hl), aes(size=pmin(500, tcga_aneup)), color="red") +
     scale_size_area() +
@@ -52,7 +65,7 @@ cmp = both %>% mutate(gene=gene, tcga_ccle = (tcga_pur + ccle)/2) %>%
     inner_join(orf2 %>% transmute(gene=gene, orf2=statistic))
 m1 = broom::glance(lm(orf1 ~ tcga_ccle, data=cmp)); m1
 m2 = broom::glance(lm(orf2 ~ tcga_ccle, data=cmp)); m2
-ggplot(cmp, aes(x=tcga_ccle, y=orf2)) +
+p4 = ggplot(cmp, aes(x=tcga_ccle, y=orf2)) +
     geom_point(aes(size=pmin(500, tcga_aneup)), alpha=0.1) +
     scale_size_area() +
     geom_smooth(method="lm") +
@@ -60,3 +73,10 @@ ggplot(cmp, aes(x=tcga_ccle, y=orf2)) +
     ggrepel::geom_label_repel(data=cmp %>% filter(gene %in% hl), #filter(tcga_ccle < -0.5, orf2 < -1),
                               aes(label=gene), size=2.5, max.overlaps = Inf) +
     labs(title = "stan-nb", subtitle=sprintf("R^2 = %.2g (z), %.2g (loess z)", m1$r.squared, m2$r.squared))
+
+pdf(args$plotfile)
+print(p1)
+print(p2)
+print(p3)
+print(p4)
+dev.off()

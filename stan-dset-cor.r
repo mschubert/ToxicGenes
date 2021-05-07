50bcc4613d2f6df4ee985ca7e0568cd449d64f08
mschubertv <mschu.dev@gmail.com>
2021-05-07 10:15:56 +0000

    upd

--- stan-dset-cor.r
+++ stan-dset-cor.r
@@ -3,30 +3,41 @@ library(ggplot2)
 
 orf1 = readxl::read_xlsx("orf/fits_naive.xlsx") %>% dplyr::rename(gene=`GENE SYMBOL`)
 orf2 = readxl::read_xlsx("orf/fits_corrected.xlsx") %>% dplyr::rename(gene=`GENE SYMBOL`)
-ccle = readRDS("ccle/pan/stan-nb/genes.rds")
+ccle = readRDS("ccle/pan/stan-nb.rds")
 tcga1 = readRDS("tcga/pan/stan-nb_naive.rds")
 tcga2 = readRDS("tcga/pan/stan-nb_pur.rds")
-tcga3 = readRDS("tcga/pan/stan-nb_puradj.rds")
+#tcga3 = readRDS("tcga/pan/stan-nb_puradj.rds")
 
 hl = c("RBM12", "RBM14", "SNRPA", "HNRNPL", "CDKN1A", "H3F3C", "DAZAP1", "BANP", "HHEX",
        "CEBPE", "HNRNPL", "HNRNPA2B1", "KLF2", "KLF4", "IRF2", "SOX15", "ELK3", "ADORA2A",
        "BCL6", "KLF12", "IFNG", "IFNA", "ZBTB14", "FOS", "PIK3IP1", "RBMS2", "KLF4", "HES1",
        "LCOR", "ZC3H10", "ALKBH7", "CREB1", "GPR3", "KLF3")
-hl = c(hl, grep("^(TR[ABG]V|IG[KL]V)", tcga1$gene, value=T))
+hl2 = grep("^(TR[ABG]V|IG[KL]V)", tcga1$gene, value=T)
 
 # tcga purity
 both = inner_join(tcga1 %>% transmute(gene=gene, tcga_naive=estimate),
-           tcga3 %>% transmute(gene=gene, tcga_pur=estimate))
+           tcga2 %>% transmute(gene=gene, tcga_pur=sign(estimate)*pmin(abs(estimate), 5)))
 ggplot(both, aes(x=tcga_naive, y=tcga_pur)) +
     geom_point(alpha=0.1) +
     geom_point(data=both %>% filter(gene %in% hl), color="red") +
+    geom_point(data=both %>% filter(gene %in% hl2), color="blue") +
+    geom_text(data=both %>% filter(gene %in% hl2), aes(label=gene), size=1.5) +
     ggrepel::geom_label_repel(data=both %>% filter(gene %in% hl), aes(label=gene), size=2.5)
 
 # tcga-ccle cor
-both = inner_join(ccle %>% transmute(gene=gene, ccle=estimate),
-           tcga1 %>% transmute(gene=gene, tcga=estimate, tcga_aneup=n_aneup))
-m0 = broom::glance(lm(ccle ~ tcga, data=both)); m0
-ggplot(both, aes(x=ccle, y=tcga)) +
+both = ccle %>% transmute(gene=gene, ccle=estimate) %>%
+    left_join(tcga1 %>% transmute(gene=gene, tcga_naive=estimate, tcga_aneup=n_aneup)) %>%
+    left_join(tcga2 %>% transmute(gene=gene, tcga_pur=estimate))
+m0 = broom::glance(lm(ccle ~ tcga_naive, data=both)); m0
+ggplot(both, aes(x=ccle, y=tcga_naive)) +
+    geom_point(aes(size=pmin(500, tcga_aneup)), alpha=0.1) +
+    geom_point(data=both %>% filter(gene %in% hl), aes(size=pmin(500, tcga_aneup)), color="red") +
+    scale_size_area() +
+    ggrepel::geom_label_repel(data=both %>% filter(gene %in% hl), aes(label=gene), size=2.5, max.overlaps=Inf) +
+    labs(title = "stan-nb", subtitle=sprintf("R^2=%.2g", m0$r.squared))
+
+m0 = broom::glance(lm(ccle ~ tcga_pur, data=both)); m0
+ggplot(both, aes(x=ccle, y=tcga_pur)) +
     geom_point(aes(size=pmin(500, tcga_aneup)), alpha=0.1) +
     geom_point(data=both %>% filter(gene %in% hl), aes(size=pmin(500, tcga_aneup)), color="red") +
     scale_size_area() +
@@ -34,7 +45,7 @@ ggplot(both, aes(x=ccle, y=tcga)) +
     labs(title = "stan-nb", subtitle=sprintf("R^2=%.2g", m0$r.squared))
 
 # tcga+ccle vs orf screen
-cmp = both %>% mutate(gene=gene, tcga_ccle = (tcga + ccle)/2) %>%
+cmp = both %>% mutate(gene=gene, tcga_ccle = (tcga_pur + ccle)/2) %>%
 #    inner_join(orf1 %>% transmute(gene=gene, orf1=estimate)) %>%
 #    inner_join(orf2 %>% transmute(gene=gene, orf2=estimate))
     inner_join(orf1 %>% transmute(gene=gene, orf1=statistic)) %>%

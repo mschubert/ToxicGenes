e512b14853788f8604ceac996a250a712e759f1c
mschubertv <mschu.dev@gmail.com>
2021-05-28 18:17:29 +0000

    small tweaks

--- candidates_mech/by_gene.r
+++ candidates_mech/by_gene.r
@@ -153,19 +153,17 @@ plot_l2d = function(dset, variable, et=0.15, from=NA, to=NA, by="purity") {
 
     if (all(na.omit(dset[[variable]]) >= 0)) {
         fill = scale_fill_viridis_c(option="magma", direction=-1, limits=c(from, to))
-        ptcol = "white"
     } else {
         fill = scale_fill_gradientn(colours=rev(RColorBrewer::brewer.pal(11,"RdBu")),
                                 limits=c(from, to))
-        ptcol = "magenta"
     }
     ggplot(dset, aes(x=cancer_copies, y=expr)) +
-        util$stat_gam2d(aes_string(fill=variable, by=by), se_alpha=TRUE, gamma=30) +
-        geom_density2d(breaks=c(0.5,0.1,0.05), color="chartreuse4", size=0.7, contour_var="ndensity") +
+        util$stat_gam2d(aes_string(fill=variable, by=by), se_alpha=TRUE) +
+        geom_density2d(breaks=c(0.5,0.15,0.05), color="chartreuse4", size=0.7, contour_var="ndensity") +
         geom_vline(xintercept=c(2-et,2+et), color="springgreen4", linetype="dotted", size=1.5) +
         facet_grid(p53_mut ~ cohort, scales="free") +
         fill +
-        geom_point(data=lowdens, color=ptcol, alpha=0.6, shape=1, size=3) +
+        geom_point(data=lowdens, color="magenta", alpha=0.6, shape=1, size=3) +
         scale_shape_manual(name="Mutation", guide="legend", na.value=21,
                            values=c(0, seq_along(levels(td$mut))[-1]),
                            labels=levels(td$mut)) +

2212a66ff592373e8a3bc09fd75e4a35174ba5a6
mschubertv <mschu.dev@gmail.com>
2021-05-28 19:22:46 +0000

    fix select-top from stan-nb

--- merge_rank/rank_top.r
+++ merge_rank/rank_top.r
@@ -3,13 +3,12 @@ sys = import('sys')
 
 score = function(dset) {
     score = dset %>%
-        filter(adj %in% c("none", "pur"), #TODO: keep "none" for TCGA too???
+        filter(adj %in% c("none", "pur", "puradj"),
                fit %in% c("lm", args$fit)) %>%
-        mutate(rsq = ifelse(dset == "orf", 0.5, rsq), #TODO: better way? (value determines ORF weight)
+        mutate(#rsq = ifelse(dset == "orf", 0.5, rsq), #TODO: better way? (value determines ORF weight)
                estimate = ifelse(dset == "orf", 2^estimate - 1, estimate),
                estimate = pmax(estimate, -1)) %>%
-        mutate(score = (1-adj.p) * (rank(-statistic) / length(statistic)) *
-                       pmax(0,rsq) * (-estimate))
+        mutate(score = (1-adj.p) * (-estimate))
 
     score %>%
         group_by(name, dset) %>% # mean by fit (rlm, rank)
@@ -24,12 +23,13 @@ score = function(dset) {
 args = sys$cmd$parse(
     opt('i', 'infile', 'dset', '../merge/pan.rds'),
     opt('f', 'fit', 'character', 'rlm3'),
-    opt('o', 'outfile', 'xlsx', 'pan.xlsx'))
+    opt('o', 'outfile', 'xlsx', 'pan.xlsx')
+)
 
 dset = readRDS(args$infile)
 top = list(
-    all = score(dset),
-    amp = score(dset %>% filter(cna %in% c("oe", "amp"))),
-    del = score(dset %>% filter(cna == "del"))
+#    all = score(dset),
+    amp = score(dset %>% filter(cna %in% c("oe", "amp")))#,
+#    del = score(dset %>% filter(cna == "del"))
 )
 writexl::write_xlsx(top, args$outfile)

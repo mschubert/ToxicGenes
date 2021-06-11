2e92ebfb9d772110e58fd686b17850446c624740
mschubertv <mschu.dev@gmail.com>
2021-06-11 16:13:04 +0000

    add transcript, cg annots

--- candidates_mech/dset_meth.r
+++ candidates_mech/dset_meth.r
@@ -1,9 +1,27 @@
 library(dplyr)
 sys = import('sys')
+seq = import('seq')
 plt = import('plot')
 tcga = import('data/tcga')
 util = import('../candidates/util')
 util2 = import('./util')
+
+plot_gene_annot = function(gene_name, cpg) {
+    trs = seq$coords$transcript() %>%
+        filter(external_gene_name == gene_name) %>%
+        transmute(label=ensembl_transcript_id, start=transcript_start, end=transcript_end)
+
+    probes = grep("^cg", colnames(cpg), value=TRUE)
+    cgs = tcga$meth_cg2gene() %>%
+        filter(probeID %in% probes) %>%
+        transmute(label=probeID, start=CpG_beg, end=CpG_end)
+
+    both = bind_rows(list(transcript=trs, cgs=cgs), .id="type")
+
+    ggplot(both, aes(x=start, y=label, color=type)) +
+        geom_segment(aes(xend=end, yend=label), size=1) +
+        geom_point()
+}
 
 meth_for_gene = function(cohorts, gene_name, gene_id="external_gene_name") {
     cg1 = tcga$meth_summary(cohorts, gene_id)[gene_name,,]
@@ -36,6 +54,7 @@ sys$run({
 
     pdf(args$plotfile, 28, 12)
     print(plt$text(sprintf("Methylation (%i CpG)", util2$nc(cpg)), size=20))
+    print(plot_gene_annot(args$gene, cpg))
     if (sum(!is.na(dset$meth_eup_scaled)) > 0)
         print(util2$plot_l2d(dset, "meth_eup_scaled"))
 

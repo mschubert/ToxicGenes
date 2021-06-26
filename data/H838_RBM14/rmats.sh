49b1df4d410260ad57d388b12e46499f4420d5cf
mschubertv <mschu.dev@gmail.com>
2021-06-26 22:35:51 +0000

    fix run

--- data/H838_RBM14/rmats.sh
+++ data/H838_RBM14/rmats.sh
@@ -2,9 +2,10 @@
 python ~/rmats-turbo/rmats.py \
     --b1 luc.txt \
     --b2 rbm.txt \
-    --gtf ./ref_annot.gtf \
-    -t paired \
-    --readLength 50 \
+    --gtf ref_annot.gtf \
+    --readLength 100 \
     --nthread 12 \
     --od rmats_out \
     --tmp rmats_tmp
+
+#-t paired \

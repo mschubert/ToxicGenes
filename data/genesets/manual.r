cb200820f149f41c94a73bdc2fea238d96620647
mschubertv <mschu.dev@gmail.com>
2022-01-19 23:25:41 +0000

    add newer corum

--- data/genesets/manual.r
+++ data/genesets/manual.r
@@ -1,16 +1,17 @@
 sys = import('sys')
 idmap = import('process/idmap')
-enr = import('tools/enrichr')
+gset = import('genesets')
 
 args = sys$cmd$parse(
     opt('h', 'haplo', 'xlsx' , 'pnas.1900437116.sd01.xlsx'),
     opt('e', 'essential', 'text file', 'depmap_essential.txt'),
     opt('n', 'nonessential', 'text file', 'depmap_nonessential.txt'),
     opt('d', 'davoli', 'xlsx', '1-s2.0-S0092867413012877-mmc2.xlsx'),
-    opt('o', 'outfile', 'save to rds', 'manual.rds'))
+    opt('o', 'outfile', 'save to rds', 'manual.rds')
+)
 
 davoli = readxl::read_xlsx(args$davoli, skip=2)
-cplx = sets = enr$genes("CORUM")
+cor = gset$get_human(c("CORUM_core", "CORUM_all", "CORUM_splice"))
 
 sets = list(
     haplo_insuff = setdiff(readxl::read_xlsx(args$haplo, "published data")$gene, "WT"),
@@ -20,8 +21,15 @@ sets = list(
             read.table(args$nonessential, header=TRUE, sep="\t")$gene),
     Davoli_oncogenes = davoli$OG,
     Davoli_TSGs = davoli$TSG,
-    CORUM_complexes= unique(unlist(cplx))
+    CORUM_all= unique(unlist(cor$CORUM_all)),
+    CORUM_core= unique(unlist(cor$CORUM_core)),
+    CORUM_splice= unique(unlist(cor$CORUM_splice))
 )
-sets = c(sets, cplx[grepl("snRNP|proteasome", names(cplx))])
+
+sets = c(
+    sets,
+    cor$CORUM_all[grepl("snRNP|proteasome", names(cor$CORUM_all)) |
+                  sapply(cor$CORUM_all, function(x) "RBM14" %in% x)]
+)
 
 saveRDS(sets, file=args$outfile)

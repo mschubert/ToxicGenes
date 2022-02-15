3feab03f486282f3878b6242c53d360432c375c8
mschubertv <mschu.dev@gmail.com>
2022-02-15 22:21:15 +0000

    nicer plots

--- data/rmats/global_ee-ei.r
+++ data/rmats/global_ee-ei.r
@@ -40,24 +40,39 @@ meta$reads = lapply(reads, . %>% as.data.frame() %>% tibble::rownames_to_column(
 saveRDS(meta, file=args$outfile)
 
 mu = tidyr::unnest(meta, reads) #%>% filter(cline == "HCC70", time == "24h")
+mu2 = mu %>% filter(ee > 50, ee < 1000) %>% mutate(ee_ei = ee/ei)
+mu2.8 = mu2 %>% filter(time == "8h", gene_name %in% sample(unique(gene_name), 1000))
+mu2.24 =mu2 %>% filter(time == "24h", gene_name %in% sample(unique(gene_name), 1000))
 
-mod8 = lm(ee_ei ~ gene_name + cond, data=mu %>% filter(ee > 500, ee < 1000, time == "8h") %>% mutate(ee_ei = ee/ei))
-mod24 = lm(ee_ei ~ gene_name + cond, data=mu %>% filter(ee > 500, ee < 1000, time == "24h") %>% mutate(ee_ei = ee/ei))
+mod8 = lm(ee_ei ~ gene_name + cond, data=mu2.8)
+mod24 = lm(ee_ei ~ gene_name + cond, data=mu2.24)
 
 broom::tidy(mod8) %>% filter(!grepl("gene_name", term))
 broom::tidy(mod24) %>% filter(!grepl("gene_name", term))
 
+lm(ee_ei ~ gene_name + cond, data=mu2.8 %>% filter(cline == "H838")) %>% broom::tidy() %>% filter(!grepl("gene_name", term))
+lm(ee_ei ~ gene_name + cond, data=mu2.8 %>% filter(cline == "H1650")) %>% broom::tidy() %>% filter(!grepl("gene_name", term))
+lm(ee_ei ~ gene_name + cond, data=mu2.8 %>% filter(cline == "HCC70")) %>% broom::tidy() %>% filter(!grepl("gene_name", term))
+
+lm(ee_ei ~ gene_name + cond, data=mu2.24 %>% filter(cline == "H838")) %>% broom::tidy() %>% filter(!grepl("gene_name", term))
+lm(ee_ei ~ gene_name + cond, data=mu2.24 %>% filter(cline == "H1650")) %>% broom::tidy() %>% filter(!grepl("gene_name", term))
+lm(ee_ei ~ gene_name + cond, data=mu2.24 %>% filter(cline == "HCC70")) %>% broom::tidy() %>% filter(!grepl("gene_name", term))
+
 pdf(args$plotfile, 10, 8)
-ggplot(mu %>% filter(ee > 1000), aes(x=rep, y=ei/ee)) +
-    geom_boxplot(aes(fill=cond), outlier.shape=NA) +
+ggplot(mu %>% filter(ee > 50), aes(x=rep, y=log2(ei/ee))) +
+    geom_hline(yintercept=0, linetype="dashed", color="grey", size=1.5) +
+    geom_boxplot(aes(fill=cond), outlier.shape=NA, alpha=0.6) +
+    scale_fill_manual(values=c(Luc="black", RBM14="red")) +
     facet_grid(cline ~ time) +
-    coord_cartesian(ylim=c(0,2))
+    coord_cartesian(ylim=c(-3,3)) +
+    theme_minimal()
 
 ggplot(mu %>% filter(ee > 5, ei > 5), aes(x=ee, y=ei)) +
-#    geom_point(aes(color=cond, shape=rep), alpha=0.02) +
     geom_density2d(aes(color=cond, linetype=rep), bins=5) +
-    scale_color_manual(values=c(Luc="grey", RBM14="red")) +
+    scale_color_manual(values=c(Luc="black", RBM14="red")) +
+    scale_linetype_manual(values=c("1"="dashed", "2"="dotted")) +
     facet_grid(cline ~ time) +
     scale_x_continuous(trans="log1p", breaks=10^(1:4), limits=c(5,1e4), expand=c(0,0)) +
-    scale_y_continuous(trans="log1p", breaks=10^(1:4), limits=c(5,1e4), expand=c(0,0))
+    scale_y_continuous(trans="log1p", breaks=10^(1:4), limits=c(5,1e4), expand=c(0,0)) +
+    theme_minimal()
 dev.off()

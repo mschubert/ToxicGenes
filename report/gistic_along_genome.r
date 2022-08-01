a42a19041e24d6860e1dddb36d061a6ae8b95aec
mschubertv <mschu.dev@gmail.com>
2022-08-01 08:46:29 +0000

    fix new gistic obj

--- report/gistic_along_genome.r
+++ report/gistic_along_genome.r
@@ -5,8 +5,8 @@ tcga = import('data/tcga')
 
 hlg = c("MYC", "EGFR", "CCND1", "CDKN1A", "TP53", "BAP1", "CDKN1A", "IL7R", "CKS1B",
         "APC", "CDKN2A", "KRAS", "NRAS", "RB1", "SMAD4", "CCNE1", "PIK3CA", "AURKA")
-hlg = readRDS("../data/genesets/manual.rds")[c("Davoli_oncogenes", "Davoli_TSGs")] %>%
-    unlist(use.names=FALSE) %>% na.omit() %>% c()
+#hlg = readRDS("../data/genesets/manual.rds")[c("Davoli_oncogenes", "Davoli_TSGs")] %>%
+#    unlist(use.names=FALSE) %>% na.omit() %>% c()
 
 gt = seq$gene_table() %>%
     transmute(chr = factor(chromosome_name, levels=c(1:22,'X')),
@@ -48,16 +48,30 @@ ggplot(cg2, aes(x=tss)) +
 pos_set_comp = readr::read_tsv("../cor_tcga_ccle/positive_comp_set.tsv") %>%
     dplyr::rename(gene_name=gene)
 hit = pos_set_comp %>% filter(hit)
-# comp selection
-gt2 = inner_join(hit, cg)
 
 # davoli
 dav = readRDS("../data/genesets/manual.rds")[c("Davoli_oncogenes", "Davoli_TSGs")] %>%
-    lapply(function(x) x[!is.na(x)]) %>% stack() %>% dplyr::rename(gene_name=values, group=ind)
-gt2 = inner_join(dav, cg) %>% as_tibble()
-ggplot(gt2, aes(x=f_amp, y=f_del)) +
+    stack() %>% dplyr::rename(gene_name=values, group=ind)
+manual = readRDS("../data/genesets/manual.rds")
+
+cg2_wide = tidyr::pivot_wider(cg2, names_from="type", values_from="frac") %>%
+    left_join(dav) %>% as_tibble() %>%
+    inner_join(pos_set_comp) %>%
+    mutate(cosmic = case_when(
+               gene_name %in% with(manual, c(Cosmic_OG_Tier1, Cosmic_OG_Tier2)) ~ "oncogene",
+               gene_name %in% with(manual, c(Cosmic_TSG_Tier1, Cosmic_TSG_Tier2)) ~ "tsg",
+               TRUE ~ NA_character_
+           ),
+           is_orf_dropout = cut(est_orf, c(-Inf,-1,-0.25,0.25,Inf),
+                                labels=c("stop+","stop","not","go")) %>% relevel("not"))
+ggplot(cg2_wide, aes(x=amplification, y=deletion)) +
+    geom_abline(intercept=0, slope=-1, color="grey", linetype="dashed") +
     geom_point(aes(color=group)) +
     ggrepel::geom_text_repel(aes(color=group,label=gene_name), size=3, max.time=10, max.iter=1e5)
+ggplot(cg2_wide, aes(x=est_orf, y=est_ccle+est_tcga, color=cosmic)) +
+    geom_point() +
+    geom_smooth(method="lm", color="black")
+#    geom_text(aes(label=gene_name))
 
 xx = right_join(dav, pos_set_comp) %>% inner_join(cg) %>% as_tibble() %>%
     select(gene_name, group, est_ccle, est_tcga, f_amp, f_del)

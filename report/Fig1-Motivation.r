199c40ae40248a96eb5654ce564e6b3e163eddae
mschubert <mschu.dev@gmail.com>
2022-08-22 12:01:40 +0200

    overview + genome

--- report/Fig1-Motivation.r
+++ report/Fig1-Motivation.r
@@ -5,6 +5,16 @@ sys = import('sys')
 seq = import('seq')
 tcga = import('data/tcga')
 
+schema = function() {
+    img = grid::rasterGrob(magick::image_read("external/comp+tox.svg"))
+    ggplot() + annotation_custom(img) + theme(panel.background=element_blank())
+}
+
+overlap = function() {
+    img = grid::rasterGrob(magick::image_read("external/overlap.svg"))
+    ggplot() + annotation_custom(img) + theme(panel.background=element_blank())
+}
+
 get_gistic_scores = function() {
     gt = seq$gene_table() %>%
         transmute(chr = factor(chromosome_name, levels=c(1:22,'X')),
@@ -64,56 +74,16 @@ get_cosmic_annot = function() {
         summarize(type = ifelse(length(type) == 1, as.character(type), "Both"))
 }
 
-og_vs_tsg = function(gistic, cosmic, hlg=c()) {
-    gwide = tidyr::pivot_wider(gistic, names_from="type", values_from="frac") %>%
-        left_join(cosmic) %>%
-        mutate(label = ifelse(!is.na(type) & gene_name %in% hlg, gene_name, NA))
-
-    p = ggplot(gwide, aes(x=amplification, y=-deletion, color=type)) +
-        geom_abline(intercept=0, slope=1, color="grey", linetype="dashed") +
-        geom_point(aes(shape=tier), na.rm=TRUE) +
-        ggrepel::geom_label_repel(aes(label=label), size=3, min.segment.length=0,
-            segment.alpha=0.3, fill="#ffffffc0", label.size=NA, na.rm=TRUE) +
-        scale_shape_manual(values=c("1"=19, "2"=1), name="Tier") +
-        scale_color_manual(values=c(Oncogene="firebrick", TSG="navy", Both="purple"), name="Type") +
-        coord_fixed() +
-        theme_classic() +
-        labs(x = "Amplification frequency",
-             y = "Deletion frequency")
-
-    gwide$type[is.na(gwide$type)] = "Background"
-    boxbee = function(y1, y2) list(
-        geom_boxplot(),
-        ggbeeswarm::geom_quasirandom(alpha=0.5),
-        ggsignif::geom_signif(comparisons=list(c("Background", "Oncogene"), c("Background", "TSG")),
-                              y_position=c(y1, y2), color="black", test=wilcox.test),
-        theme_classic(),
-        labs(x = "Gene type"),
-        theme(axis.text.x = element_blank())
-    )
-    pa = ggplot(gwide, aes(x=type, y=amplification, color=type)) +
-        boxbee(0.48, 0.52) + ylab("Amplification frequency") +
-        geom_hline(yintercept=median(gwide$amplification[gwide$type=="Background"]),
-                   linetype="dashed", color="black")
-    pd = ggplot(gwide, aes(x=type, y=-deletion, color=type)) +
-        boxbee(0.4, 0.44) + ylab("Deletion frequency") +
-        geom_hline(yintercept=-median(gwide$deletion[gwide$type=="Background"]),
-                   linetype="dashed", color="black")
-
-    p | (pa | pd) + plot_layout(guides="collect")
-}
-
 sys$run({
     hlg = c("MYC", "EGFR", "CCND1", "CDKN1A", "TP53", "BAP1", "CDKN1A", "IL7R", "CKS1B",
             "APC", "CDKN2A", "KRAS", "NRAS", "RB1", "SMAD4", "CCNE1", "PIK3CA", "AURKA")
 
     gistic = get_gistic_scores()
-    cosmic = get_cosmic_annot()
 
-    top = cna_along_genome(gistic, hlg)
-    btm = og_vs_tsg(gistic, cosmic, hlg)
+    top = (schema() | overlap()) + plot_layout(widths=c(3,2))
+    btm = wrap_elements(cna_along_genome(gistic, hlg))
 
-    asm = (top / btm) + plot_layout(heights=c(3,4)) + plot_annotation(tag_levels='a') &
+    asm = (top / btm) + plot_layout(heights=c(3,2)) + plot_annotation(tag_levels='a') &
         theme(plot.tag = element_text(size=18, face="bold"))
 
     pdf("Fig1-Motivation.pdf", 14, 8)

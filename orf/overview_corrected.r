c871a55585f913d3824f9638e34b11d7c9d57f43
mschubert <mschu.dev@gmail.com>
2022-08-26 15:27:42 +0200

    move plot for util use

--- orf/overview_corrected.r
+++ orf/overview_corrected.r
@@ -3,20 +3,6 @@ io = import('io')
 sys = import('sys')
 ov = import('./overview_naive')
 
-plot_overview = function(data, title, label_top=20) {
-    data = data %>%
-        mutate(label = ifelse(rank(z_LFC) <= label_top, `GENE SYMBOL`, NA))
-    ggplot(data, aes(x=DMSO, y=z_LFC)) +
-        geom_point(alpha=0.5) +
-        ov$loess_sd(data$DMSO, data$z_LFC) +
-#        stat_loess_sd(color="red", size=1) +
-        ggrepel::geom_label_repel(aes(label=label), size=2) +
-        labs(title = title,
-             x = "DMSO",
-             y = "LFC DMSO/ETP z-score") +
-        theme_classic()
-}
-
 args = sys$cmd$parse(
     opt('i', 'infile', 'rds', 'overview.rds'),
     opt('p', 'plotfile', 'pdf', 'overview_corrected.pdf'))
@@ -27,7 +13,7 @@ percell = expr %>%
     mutate(cells = sprintf("%s (%s)", cells, tissue)) %>%
     group_by(cells) %>%
     tidyr::nest() %>%
-    mutate(plot = purrr::map2(data, cells, plot_overview))
+    mutate(plot = purrr::map2(data, cells, ov$plot_overview_corrected))
 
 pdf(args$plotfile)
 for (p in percell$plot)
--- orf/overview_naive.r
+++ orf/overview_naive.r
@@ -50,6 +50,20 @@ plot_overview = function(data, title, label_top=20) {
              y = "LFC DMSO/ETP")
 }
 
+plot_overview_corrected = function(data, title, label_top=20) {
+    data = data %>%
+        mutate(label = ifelse(rank(z_LFC) <= label_top, `GENE SYMBOL`, NA))
+    ggplot(data, aes(x=DMSO, y=z_LFC)) +
+        geom_point(alpha=0.5) +
+        loess_sd(data$DMSO, data$z_LFC) +
+#        stat_loess_sd(color="red", size=1) +
+        ggrepel::geom_label_repel(aes(label=label), size=2) +
+        labs(title = title,
+             x = "DMSO",
+             y = "LFC DMSO/ETP z-score") +
+        theme_classic()
+}
+
 if (is.null(module_name())) {
     args = sys$cmd$parse(
         opt('t', 'tissues', 'txt', '../data/orf/tissues.txt'),

57162ff9c0c32883657b7b13c66f55e44c375e63
mschubert <mschu.dev@gmail.com>
2022-08-30 10:40:29 +0200

    og/tsg with inset

--- report/FigS1-OG_TSG.r
+++ report/FigS1-OG_TSG.r
@@ -3,16 +3,31 @@ library(ggplot2)
 library(patchwork)
 sys = import('sys')
 seq = import('seq')
+plt = import('plot')
 tcga = import('data/tcga')
 cm = import('./common')
 
-og_vs_tsg = function(gistic, cosmic, hlg=c()) {
+og_vs_tsg = function(gistic, cosmic) {
+    sets = list(
+        Oncogene = cosmic$gene_name[grepl("^O", cosmic$type)],
+        TSG = cosmic$gene_name[grepl("TSG", cosmic$type)]
+    )
+    venn = plt$venn(sets, label=FALSE) +
+        scale_fill_manual(values=cm$cols[c("Oncogene", "TSG")]) +
+        annotate("text", x=0, y=0, label=length(intersect(sets$Oncogene, sets$TSG))) +
+        plot_layout(tag_level="new")
+
     gwide = tidyr::pivot_wider(gistic, names_from="type", values_from="frac") %>%
         left_join(cosmic) %>%
-        mutate(label = ifelse(!is.na(type) & gene_name %in% hlg, gene_name, NA))
+        mutate(label = ifelse(!is.na(type) & gene_name %in% cm$hlg, gene_name, NA))
 
-    p = ggplot(gwide, aes(x=amplification, y=-deletion, color=type)) +
-        geom_abline(intercept=0, slope=1, color="grey", linetype="dashed") +
+    areas = data.frame(xmin=c(0.15,-Inf), xmax=c(Inf,Inf), ymin=c(-Inf,0.15), ymax=c(Inf,Inf),
+        status=c("Deleted", "Amplified"))
+
+    ggplot(gwide, aes(x=-deletion, y=amplification, color=type)) +
+        geom_rect(data=areas, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=status),
+            linetype="dashed", alpha=0.1, inherit.aes=FALSE) +
+        scale_fill_manual(values=cm$cols[c("Amplified", "Deleted")], name="Frequently") +
         geom_point(aes(shape=tier), na.rm=TRUE) +
         ggrepel::geom_label_repel(aes(label=label), size=3, min.segment.length=0,
             segment.alpha=0.3, fill="#ffffffc0", label.size=NA, na.rm=TRUE) +
@@ -20,43 +35,27 @@ og_vs_tsg = function(gistic, cosmic, hlg=c()) {
         scale_color_manual(values=cm$cols[c("Oncogene", "TSG", "OG+TSG")], name="Driver status") +
         coord_fixed() +
         theme_classic() +
-        labs(x = "Amplification frequency",
-             y = "Deletion frequency")
-
-    gwide$type[is.na(gwide$type)] = "Background"
-    gwide$type = factor(gwide$type, levels=c("Background", "Oncogene", "TSG", "OG+TSG"))
-    boxbee = function(y1, y2) list(
-        geom_boxplot(outlier.shape=NA),
-        scale_fill_manual(values=cm$cols[levels(gwide$type)]),
-        ggsignif::geom_signif(comparisons=list(c("Background", "Oncogene"), c("Background", "TSG")),
-                              y_position=c(y1, y2), color="black", test=wilcox.test),
-        coord_cartesian(ylim=c(NA, max(c(y1, y2))+0.05)),
-        theme_classic(),
-        labs(x = "Gene type"),
-        theme(axis.text.x = element_blank())
-    )
-    pa = ggplot(gwide, aes(x=type, y=amplification, fill=type)) +
-        boxbee(0.45, 0.52) + ylab("Frequency") + ggtitle("Amplifications") +
-        geom_hline(yintercept=median(gwide$amplification[gwide$type=="Background"]),
-                   linetype="dashed", color="black")
-    pd = ggplot(gwide, aes(x=type, y=-deletion, fill=type)) +
-        boxbee(0.4, 0.47) + ylab("Frequency") + ggtitle("Deletions") +
-        geom_hline(yintercept=-median(gwide$deletion[gwide$type=="Background"]),
-                   linetype="dashed", color="black")
-
-    (p | (pa / pd) + plot_layout(guides="collect")) + plot_layout(widths=c(2,1))
+        labs(x = "Deletion frequency",
+             y = "Amplification frequency") +
+        inset_element(venn, right=0.95, top=1, left=0.5, bottom=0.6)
 }
+
+# venn overlap fAmp + OG/TSG
+
+# DE normal-cancer for OG/TSG
+
+# RPE-1 common parental DE with amps?
 
 sys$run({
     gistic = readRDS("../data/gistic_smooth.rds")$genes
     cosmic = cm$get_cosmic_annot()
 
-    btm = og_vs_tsg(gistic, cosmic, cm$hlg)
+    btm = og_vs_tsg(gistic, cosmic)
 
     asm = btm + plot_annotation(tag_levels='a') &
         theme(plot.tag = element_text(size=18, face="bold"))
 
-    pdf("FigS1-OG_TSG.pdf", 10, 5.5)
+    pdf("FigS1-OG_TSG.pdf", 6, 5.5)
     print(asm)
     dev.off()
 })

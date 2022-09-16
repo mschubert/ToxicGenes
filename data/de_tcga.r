dba35e35f0948348c3084c6e7599693bad50bb40
mschubertv <mschu.dev@gmail.com>
2022-09-16 08:07:09 +0000

    add normal-cancer DE

--- data/de_tcga.r
+++ data/de_tcga.r
@@ -1,47 +1,31 @@
-library(igraph)
 library(dplyr)
 sys = import('sys')
 tcga = import('data/tcga')
 idmap = import('process/idmap')
+deseq = import('process/deseq')
+
+normal_cancer_de = function(cohort) {
+    message(cohort)
+    reads = tcga$rna_seq(cohort) %>% tcga$filter(primary=TRUE)
+    reads = reads[,!duplicated(colnames(reads))]
+    reads = reads[,substr(colnames(reads), 14, 16) %in% c("01A", "11A")]
+    cols = tcga$barcode2index(colnames(reads)) %>%
+        mutate(Sample.Definition = factor(Sample.Definition,
+            levels=c("Solid Tissue Normal", "Primary Solid Tumor")))
+    if (any(cols$Sample.Definition == "Solid Tissue Normal"))
+        DESeq2::DESeqDataSetFromMatrix(reads, cols, ~Sample.Definition) %>%
+            DESeq2::estimateSizeFactors() %>%
+            DESeq2::DESeq() %>%
+            deseq$extract_result() %>%
+            arrange(padj, pvalue)
+}
 
 args = sys$cmd$parse(
-    opt('o', 'outfile', 'rds', 'df_tcga.rds')
+    opt('o', 'outfile', 'rds', 'de_tcga.rds')
 )
 
 cohorts = tcga$cohorts()
-# excl x,y chroms
+res = sapply(cohorts, normal_cancer_de, simplify=FALSE)
+res = res[!sapply(res, is.null)]
 
-purity = tcga$purity() %>%
-    filter(!is.na(estimate)) %>%
-    transmute(sample = Sample,
-              purity = estimate)
-
-#mut = tcga$mutations()
-#net = OmnipathR::interaction_graph(OmnipathR::import_all_interactions())
-#as_ids(neighbors(net, "CDKN1A", "total"))
-
-reads = lapply(cohorts, tcga$rna_seq) %>%
-    narray::stack(along=2) %>%
-    tcga$filter(cancer=TRUE, primary=TRUE)
-rownames(reads) = idmap$gene(rownames(reads), to="hgnc_symbol")
-reads = reads[rowMeans(reads) >= 10 & !is.na(rownames(reads)),]
-eset = DESeq2::DESeqDataSetFromMatrix(reads, tibble(sample=colnames(reads)), ~1) %>%
-    DESeq2::estimateSizeFactors() #todo: 16 duplicate hgnc_symbol
-
-copies = lapply(cohorts, tcga$cna_genes) %>%
-    narray::stack(along=2)
-rownames(copies) = idmap$gene(rownames(copies), to="hgnc_symbol")
-narray::intersect(reads, copies, along=1)
-narray::intersect(reads, copies, along=2)
-names(dimnames(reads)) = names(dimnames(copies)) = c("gene", "sample")
-
-df = inner_join(reshape2::melt(reads, value.name="expr"),
-                reshape2::melt(copies, value.name="copies")) %>%
-    inner_join(purity) %>%
-    inner_join(as.data.frame(SummarizedExperiment::colData(eset))) %>%
-    na.omit() %>%
-    dplyr::rename(sf = sizeFactor) %>%
-    mutate(covar = tcga$barcode2study(sample),
-           cancer_copies = pmax(0, (copies-2) / purity + 2))
-
-saveRDS(df, file=args$outfile)
+saveRDS(res, file=args$outfile)

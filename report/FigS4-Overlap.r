182ea6b8e56f558610012f7c4dc13565b398668b
mschubert <mschu.dev@gmail.com>
2022-12-02 15:56:16 +0100

    add GO overlap

--- report/FigS4-Overlap.r
+++ report/FigS4-Overlap.r
@@ -32,8 +32,33 @@ comp_orf = function(all, gistic_amp) {
         annotate("text", y=10, x=0.6, hjust=0, label=lab, color="blue", parse=TRUE) +
         geom_smooth(method="lm", se=FALSE) +
         theme_classic() +
+        coord_cartesian(clip="off") +
         labs(x = "Mean expression over expected CCLE/TCGA",
              y = "ORF log fold-chance (Wald statistic)")
+}
+
+go_cors = function() {
+    orf = readxl::read_xlsx("../orf/pan/GO_Biological_Process_2021.xlsx")
+    ccle_go = readRDS("../ccle/pan/stan-nb/all/GO_Biological_Process_2021.rds")$amp %>%
+        select(label, stat_ccle=statistic)
+    tcga_go = readRDS("../tcga/pan/stan-nb_puradj/all/GO_Biological_Process_2021.rds")[[1]] %>%
+        select(label, stat_tcga=statistic, size_used)
+    both = inner_join(ccle_go, tcga_go) %>% filter(size_used < 1000) %>%
+        inner_join(orf %>% select(label=name, stat_orf=statistic)) %>%
+        mutate(tcga_ccle = (stat_ccle+stat_tcga)/2)
+
+    m = broom::glance(lm(tcga_ccle ~ stat_orf, data=both))
+    lab = sprintf("R^2~`=`~%.2f~\n~p~`=`~%.1g", m$adj.r.squared, m$p.value) %>%
+        sub("e", "%*%10^", .)
+
+    plt$denspt(both, aes(x=tcga_ccle, y=stat_orf, label=label), size=size_used,
+               max_ov=30, draw_label=120) +
+        scale_size_area(max_size=8, breaks=c(10,100,500,1000), name="Genes in set") +
+        theme_minimal() +
+        ylim(c(-25,NA)) +
+        annotate("text", x=6, y=-11, color="blue", label=lab, parse=TRUE) +
+        labs(x = "Mean expression over expected CCLE/TCGA (Wald statistic)",
+             y = "Mean dropout ORF screen (Wald statistic)")
 }
 
 sys$run({
@@ -43,10 +68,14 @@ sys$run({
     cosmic = cm$get_cosmic_annot()
     all = readr::read_tsv("../cor_tcga_ccle/positive_comp_set.tsv")
 
-    asm = (comp_orf(all, gistic_amp) | plot_spacer()) + plot_annotation(tag_levels='a') &
+    top = (comp_orf(all, gistic_amp) | plot_spacer()) + plot_layout(widths=c(3,2))
+    mid = go_cors()
+
+    asm = (top / mid) + plot_layout(heights=c(1,2)) +
+        plot_annotation(tag_levels='a') &
         theme(plot.tag = element_text(size=18, face="bold"))
 
-    pdf("FigS4-Overlap.pdf", 14, 6)
+    pdf("FigS4-Overlap.pdf", 11, 14)
     print(asm)
     dev.off()
 })

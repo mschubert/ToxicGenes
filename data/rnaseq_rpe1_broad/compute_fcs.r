192d8574b956bd01a35f0439e62c17645cbdb122
mschubertv <mschu.dev@gmail.com>
2023-01-12 16:10:49 +0000

    load star and basic qc

---
+++ data/rnaseq_rpe1_broad/compute_fcs.r
@@ -0,0 +1,34 @@
+library(dplyr)
+library(SummarizedExperiment)
+sys = import('sys')
+plt = import('plot')
+gset = import('genesets')
+idmap = import('process/idmap')
+deseq = import('process/deseq')
+dori = import('../../../dorine/data/rnaseq_adaptation/counts')
+
+# merge count files present here
+reads = list.files("aligned", "ReadsPerGene\\.out\\.tab", full.names=TRUE) %>%
+    dori$read_files() %>%
+    mutate(sample = sub(".*_([0-9A-Z]+)$", "\\1", sample))
+stats = dori$read_stats(reads)
+
+# do standard qc plots
+sel = stats %>%
+    group_by(count_type) %>%
+        summarize(part_mapped = sum(reads[feature == "N_mapped"]) / sum(reads)) %>%
+    slice_max(part_mapped) %>%
+    pull(count_type)
+levels(stats$count_type)[levels(stats$count_type) == sel] = sprintf("%s (selected)", sel)
+dori$read_barplot(stats)
+
+cmat = dori$read_matrix(reads, sel)
+meta = data.frame(sample=colnames(cmat))
+eset = DESeq2::DESeqDataSetFromMatrix(cmat, meta, ~1)
+plt$pca(eset) +
+    geom_point() +
+    geom_text(aes(label=sample))
+
+# add dorine's RPE1s to combined DESeq data set
+
+# compute DE over the ref sample

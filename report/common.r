111e6b718d5255eca9171d3e442b0bd8c90bc8d2
mschubert <mschu.dev@gmail.com>
2023-03-28 11:32:49 +0200

    add hallmark column

--- report/common.r
+++ report/common.r
@@ -5,6 +5,13 @@ hlg = c("MYC", "EGFR", "CCND1", "CDKN1A", "TP53", "BAP1", "CDKN1A", "IL7R", "CKS
 
 get_cosmic_annot = function() {
     manual = readRDS("../data/genesets/manual.rds")
+    hms = manual[grepl("Cosmic_(OG|TSG)_Hallmark", names(manual))] %>%
+        stack() %>% as_tibble() %>%
+        transmute(gene_name = values,
+                  type = case_when(
+                      grepl("OG", ind) ~ "Oncogene",
+                      grepl("TSG", ind) ~ "TSG"),
+                  hallmark = TRUE)
     manual[grepl("Cosmic_(OG|TSG)_Tier", names(manual))] %>%
         stack() %>% as_tibble() %>%
         transmute(gene_name = values,
@@ -15,7 +22,10 @@ get_cosmic_annot = function() {
                   tier = sub(".*Tier([12])$", "\\1", ind)) %>%
         distinct() %>%
         group_by(gene_name, tier) %>%
-        summarize(type = ifelse(length(type) == 1, as.character(type), "OG+TSG"))
+            summarize(type = ifelse(length(unique(type)) == 1, as.character(type), "OG+TSG")) %>%
+        ungroup() %>%
+        left_join(hms) %>%
+        mutate(hallmark = ifelse(is.na(hallmark), "No", "Yes"))
 }
 
 cols = c(

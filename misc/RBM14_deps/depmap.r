d89bf10794243e44298301000856bc2c89a27f69
mschubertv <mschu.dev@gmail.com>
2023-04-21 12:00:24 +0000

    add depmap results, plot

--- misc/RBM14_deps/depmap.r
+++ misc/RBM14_deps/depmap.r
@@ -2,17 +2,29 @@ library(dplyr)
 library(depmap)
 library(ExperimentHub)
 sys = import('sys')
+plt = import('plot')
 
-calc_assocs = function() {
-    res = dset %>%
+calc_assocs = function(dset, field) {
+    message(as.character(dset), " @ ", field)
+    res = get(dset) %>%
         select(depmap_id, gene_name, dependency) %>%
         filter(!is.na(dependency)) %>%
         inner_join(meta) %>%
+        mutate(field := !! rlang::sym(field)) %>%
         group_by(gene_name) %>%
-        filter(n_distinct(lineage) > 1) %>%
-        summarize(mod = list(broom::tidy(lm(dependency ~ lineage + rna_expression)))) %>%
-        tidyr::unnest()
+            filter(!is.na(dependency), !is.na(field)) %>%
+            filter(n_distinct(lineage) > 1) %>%
+            summarize(mod = list(broom::tidy(lm(dependency ~ lineage + field)))) %>%
+        tidyr::unnest(mod) %>%
+        filter(term == "field") %>%
+        mutate(p.adj = p.adjust(p.value, method="fdr")) %>%
+        arrange(p.adj, p.value)
 }
+
+args = sys$cmd$parse(
+    opt('o', 'outfile', 'rds', 'depmap.rds'),
+    opt('p', 'plotfile', 'pdf', 'depmap.pdf')
+)
 
 tpm = depmap::depmap_TPM() %>%
     filter(gene_name == "RBM14") %>%
@@ -25,5 +37,18 @@ meta = depmap::depmap_metadata() %>%
     inner_join(tpm) %>%
     inner_join(copy)
 
-screen = depmap::depmap_rnai()
-ko = depmap::depmap_crispr()
+rnai = depmap::depmap_rnai()
+crispr_ko = depmap::depmap_crispr()
+
+idx = tidyr::crossing(tibble(dset = c("rnai", "crispr_ko")),
+                      tibble(field = c("rna_expression", "log_copy_number"))) %>%
+    rowwise() %>%
+    mutate(res = list(calc_assocs(dset, field)),
+           plot = list(plt$volcano(res) + ggtitle(paste(dset, field))))
+
+pdf(args$plotfile)
+for (p in idx$plot)
+    print(p)
+dev.off()
+
+saveRDS(idx, args$outfile)

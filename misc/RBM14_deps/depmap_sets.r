850ca2037a968c686b5073e548c4b4756e3267d8
mschubertv <mschu.dev@gmail.com>
2023-04-24 18:03:44 +0000

    add dorothea, corum sets

--- misc/RBM14_deps/depmap_sets.r
+++ misc/RBM14_deps/depmap_sets.r
@@ -10,23 +10,25 @@ args = sys$cmd$parse(
     opt('p', 'plotfile', 'pdf', 'depmap_sets.pdf')
 )
 
+sets = c("MSigDB_Hallmark_2020", "DoRothEA", "GO_Biological_Process_Tree", "CORUM_all")
+
 idx = readRDS(args$infile) %>%
     filter(dset %in% c("rnai", "crispr_ko")) %>%
     dplyr::rename(genes = res) %>%
-    deseq$sets(c("MSigDB_Hallmark_2020", "GO_Biological_Process_Tree"), cl=5)
+    deseq$sets(sets, cl=5)
 
 hl = gset$get_human("MSigDB_Hallmark_2020")[["Oxidative Phosphorylation"]]
 idx$genes = lapply(idx$genes, . %>% mutate(circle = label %in% hl))
 
 plots = idx %>%
-    tidyr::pivot_longer(c("genes", "MSigDB_Hallmark_2020", "GO_Biological_Process_Tree")) %>%
+    tidyr::pivot_longer(all_of(c("genes", sets))) %>%
     rowwise() %>%
     mutate(plot = list(plt$volcano(value) + ggtitle(sprintf("%s (%s)", name, cond)))) %>%
     group_by(dset, field) %>%
     summarize(asm = list((plt$text(sprintf("%s :: %s", dset[1], field[1])) /
-                          wrap_plots(plot)) + plot_layout(heights=c(1,20))))
+                          wrap_plots(plot, nrow=2)) + plot_layout(heights=c(1,20))))
 
-pdf(args$plotfile, 15, 10)
+pdf(args$plotfile, 22, 10)
 for (p in plots$asm)
     print(p)
 dev.off()

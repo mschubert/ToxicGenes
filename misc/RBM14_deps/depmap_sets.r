bc6a8882c37b7e92b24861fa541dd090ae449346
mschubertv <mschu.dev@gmail.com>
2023-04-24 13:53:14 +0000

    highlight oxphos genes

--- misc/RBM14_deps/depmap_sets.r
+++ misc/RBM14_deps/depmap_sets.r
@@ -3,6 +3,7 @@ library(patchwork)
 sys = import('sys')
 plt = import('plot')
 deseq = import('process/deseq')
+gset = import('genesets')
 
 args = sys$cmd$parse(
     opt('i', 'infile', 'rds', 'depmap.rds'),
@@ -13,6 +14,9 @@ idx = readRDS(args$infile) %>%
     filter(dset %in% c("rnai", "crispr_ko")) %>%
     dplyr::rename(genes = res) %>%
     deseq$sets(c("MSigDB_Hallmark_2020", "GO_Biological_Process_Tree"), cl=5)
+
+hl = gset$get_human("MSigDB_Hallmark_2020")[["Oxidative Phosphorylation"]]
+idx$genes = lapply(idx$genes, . %>% mutate(circle = label %in% hl))
 
 plots = idx %>%
     tidyr::pivot_longer(c("genes", "MSigDB_Hallmark_2020", "GO_Biological_Process_Tree")) %>%

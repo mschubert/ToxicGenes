bf6fca24fa8d29651249053f619064b3c64246ee
mschubertv <mschu.dev@gmail.com>
2023-07-06 14:26:08 +0000

    add breast, lung tissues in plot

--- data/ccle/protein_quant.r
+++ data/ccle/protein_quant.r
@@ -15,12 +15,19 @@ pmat = prot[,c("Gene_Symbol", grep("_TenPx[0-9]+$", colnames(prot), value=TRUE))
 
 saveRDS(pmat, file="protein_quant.rds")
 
-cur = pmat %>% filter(Gene_Symbol %in% c("CDKN1A", "RBM14"))
-pdf("protein_quant.pdf", 8, 4)
+sel = pmat %>% filter(Gene_Symbol %in% c("CDKN1A", "RBM14"))
+cur = list(`Pan-can` = sel,
+           Breast = sel %>% filter(grepl("BREAST", cline)),
+           Lung = sel %>% filter(grepl("LUNG", cline))) %>%
+    bind_rows(.id="Tissue") %>%
+    mutate(Tissue = factor(Tissue, levels=c("Pan-can", "Breast", "Lung")))
+
+pdf("protein_quant.pdf", 8, 5)
 ggplot(cur, aes(x=copies, y=protein)) +
     geom_hline(yintercept=0, color="grey", size=1, linetype="dashed") +
-    geom_point(alpha=0.7) +
+    geom_point(alpha=0.6) +
     geom_smooth(method="lm", se=FALSE) +
     theme_minimal() +
-    facet_wrap(~ Gene_Symbol)
+    facet_grid(Gene_Symbol ~ Tissue) +
+    ylab("normalized protein expression")
 dev.off()

615c96cd57aeeb609196c372e7498c9b6d1ea0c8
mschubert <mschu.dev@gmail.com>
2023-09-26 16:41:09 +0200

    name 'Pan-Cancer', refactor

--- report/SuppTables.r
+++ report/SuppTables.r
@@ -1,20 +1,24 @@
 library(dplyr)
 
+load_comp = function(fname) {
+    dset = readRDS(fname) %>%
+        mutate(compensation = (1 - p.value) * estimate)
+}
+
 ccle = list.files("../model_compensation/fit_ccle-amp", recursive=TRUE, full.names=TRUE) %>%
-    setNames(tools::file_path_sans_ext(basename(.))) %>%
-    lapply(readRDS) %>%
-    lapply(. %>% mutate(compensation = (1 - p.value) * estimate))
-writexl::write_xlsx(ccle, "TableS1_CCLE-comp.xlsx")
+    setNames(tools::file_path_sans_ext(basename(.))) %>% lapply(load_comp)
+names(ccle)[names(ccle) == "pan"] = "Pan-Cancer"
 
 tcga = list.files("../model_compensation/fit_tcga_puradj-amp", recursive=TRUE, full.names=TRUE) %>%
-    setNames(tools::file_path_sans_ext(basename(.))) %>%
-    lapply(readRDS) %>%
-    lapply(. %>% mutate(compensation = (1 - p.value) * estimate))
-writexl::write_xlsx(tcga, "TableS2_TCGA-comp.xlsx")
+    setNames(tools::file_path_sans_ext(basename(.))) %>% lapply(load_comp)
+names(tcga)[names(tcga) == "pan"] = "Pan-Cancer"
+
+writexl::write_xlsx(ccle[c("Pan-Cancer", sort(setdiff(names(ccle), "Pan-Cancer")))], "TableS1_CCLE-comp.xlsx")
+writexl::write_xlsx(tcga[c("Pan-Cancer", sort(setdiff(names(tcga), "Pan-Cancer")))], "TableS2_TCGA-comp.xlsx")
 
 pan = readxl::read_xlsx("../model_orf/fits_naive.xlsx", sheet="pan")
 cline = sapply(readxl::excel_sheets("../model_orf/fits_per_screen.xlsx"), readxl::read_xlsx,
                path="../model_orf/fits_per_screen.xlsx", simplify=FALSE)
-orf = c(pan=list(pan), cline) %>%
-    lapply(. %>% dplyr::rename(gene_name = `GENE SYMBOL`) %>% filter(gene_name != "LOC254896"))
+orf = c(`Pan-Cancer`=list(pan), cline) %>%
+    lapply(. %>% dplyr::rename(gene = `GENE SYMBOL`) %>% filter(gene != "LOC254896"))
 writexl::write_xlsx(orf, "TableS3_ORF-toxicity.xlsx")

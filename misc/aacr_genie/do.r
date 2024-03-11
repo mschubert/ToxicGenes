86a2c275e9e209b4537e1331b129c6436acee580
Michael Schubert <mschu.dev@gmail.com>
2024-03-11 14:46:52 +0000

    try genie data (no rbm14 copy data)

---
+++ misc/aacr_genie/do.r
@@ -0,0 +1,18 @@
+library(dplyr)
+library(ggplot2)
+
+# set SYNAPSE_USERNAME, SYNAPSE_PASSWORD in env vars
+# request access to the 'genie nsclc'/'genie crc' on synapse
+nsclc = genieBPC::pull_data_synapse("NSCLC", version="v2.0-public")[[1]]
+#crc = genieBPC::pull_data_synapse("CRC", version="v2.0-public")
+
+# no rbm14 samples
+cna = nsclc$cna |>
+    filter(Hugo_Symbol %in% c("CCND1", "RBM14")) |>
+    tidyr::pivot_longer(-Hugo_Symbol, names_to="sample", values_to="copy")
+
+clin = nsclc$pt_char |>
+    transmute(sex = factor(naaccr_sex_code),
+              age = age_last_fu_yrs,
+              status = as.integer(!is.na(age_death_yrs)),
+              time = ??)

5f639f8bdb115e3c7af2cd700a5d8da09a9ae128
Michael Schubert <mschu.dev@gmail.com>
2024-04-11 13:26:12 +0000

    fix level5 analysis

--- misc/reviewer_lincs/level5.r
+++ misc/reviewer_lincs/level5.r
@@ -8,17 +8,18 @@ meta = readr::read_tsv("siginfo_beta.txt") |>
            pert_time == "48", # 72/96h: few genes
            qc_pass == 1)
 mat = io$load_gctx("level5_beta_trt_oe_n34171x12328.gctx")
-genes = readr::read_tsv("genes.tsv.txt") |>
-    filter(`Entrez ID` %in% rownames(mat@mat),
-           Type == "landmark")
+genes = readr::read_tsv("geneinfo_beta.txt") |> filter(feature_space == "landmark")
 
-dset = cbind(genes["Symbol"], mat@mat[as.character(genes$`Entrez ID`),]) |>
-    tidyr::pivot_longer(-Symbol, names_to="sig_id", values_to="beta") |>
+dset = mat@mat[as.character(genes$gene_id), meta$sig_id] |>
+    cbind(genes["gene_symbol"]) |>
+    tidyr::pivot_longer(-gene_symbol, names_to="sig_id", values_to="zscore") |>
     inner_join(meta) |>
-    filter(Symbol == cmap_name)
+    filter(gene_symbol == cmap_name)
 
-ggplot(dset, aes(x=forcats::fct_reorder(cmap_name, -beta), y=beta)) +
+pdf("level5.pdf")
+ggplot(dset, aes(x=forcats::fct_reorder(cmap_name, -zscore), y=zscore)) +
     geom_boxplot() +
     facet_wrap(~ cell_mfc_name + pert_idose + pert_itime) +
     coord_flip() +
     labs(x = "gene")
+dev.off()

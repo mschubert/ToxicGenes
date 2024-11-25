cc3bd9e45eb0b523ae103957aa7df074434d40e5
Michael Schubert <mschu.dev@gmail.com>
2024-11-25 15:36:23 +0100

    incl resp curves + diff norms

--- misc/STAT3_inhibitors/do.r
+++ misc/STAT3_inhibitors/do.r
@@ -22,35 +22,64 @@ load_sheet = function(sheet) {
     bind_rows(ir0, ir1)
 }
 
+resp_curves = function(dset) {
+    ggplot(dset, aes(x=conc, y=value, color=oex, group=paste(sample, oex, IR, rep))) +
+        geom_line(aes(linetype=IR)) +
+        facet_grid(drug ~ cell_line)
+}
+
+luc_vs_rbm_1uM = function(dset) {
+    ggplot(dset |> filter(conc == "1uM"), aes(x=oex, fill=oex, y=value)) +
+        geom_hline(yintercept=1, linetype="dashed", color="black") +
+        geom_boxplot(alpha=0.7) +
+        geom_line(aes(group=paste(sample, drug, IR), linetype=IR), alpha=0.15) +
+        ggbeeswarm::geom_quasirandom(aes(shape=IR), width=0.2, alpha=0.9) +
+        facet_grid(drug ~ cell_line) +
+        ggsignif::geom_signif(y_position=1.2, color="black", tip_length=0,
+            test=function(...) t.test(..., paired=TRUE),
+            map_signif_level=cm$fmt_p, parse=TRUE,
+            comparisons=list(c("Luc", "RBM14"))) +
+        scale_shape_manual(values=c(21,22)) +
+        coord_cartesian(ylim=c(0.5, 1.3)) +
+        scale_fill_brewer(palette="Set1") +
+        labs(x = "Gene overexpressed",
+             y = "Relative viability 1 uM drug vs. DMSO",
+             shape = "IR",
+             fill = "Gene\noverexpressed") +
+        cm$theme_minimal()
+}
+
 fname = "RBM14 data（calculation）Normalize to Luciferase.xlsx"
 sheets = readxl::excel_sheets(fname) |> setdiff("Sum up")
 dset = sapply(sheets, load_sheet, simplify=FALSE) |>
     bind_rows(.id="sample") |>
     mutate(cell_line = sub("([^ ]+) ([0-9]+)", "\\1", sample),
-           rep = sub("([^ ]+) ([0-9]+)", "\\2", sample))
+           conc = factor(conc, levels=c("0uM", "0.1uM", "1uM", "10uM")),
+           rep = sub("([^ ]+) ([0-9]+)", "\\2", sample)) |>
+    group_by(sample, drug, IR) |>
+        mutate(value = value / mean(value[conc == "0uM" & oex == "Luc"])) |>
+    ungroup()
 
 dset2 = dset |>
-    group_by(cell_line, drug, oex, IR) |>
-    mutate(value = value / value[conc == "0uM"]) |>
-    filter(conc == "1uM")
+    group_by(sample, drug, oex, IR) |>
+        mutate(value = value / mean(value[conc == "0uM"])) |>
+    ungroup()
+
+# 0 uM in HCC70 is unreliable, plate effect?
+dset3 = dset |>
+    filter(! (cell_line == "HCC70" & conc == "0uM")) |>
+    mutate(conc = case_when(
+        cell_line == "HCC70" & conc == "0.1uM" ~ factor("0uM"),
+        TRUE ~ conc
+    )) |>
+    group_by(sample, drug, oex, IR) |>
+        mutate(value = value / mean(value[conc == "0uM"])) |>
+    ungroup()
 
 pdf("Rplots.pdf", 7, 5)
-ggplot(dset2, aes(x=oex, fill=oex, y=value)) +
-    geom_hline(yintercept=1, linetype="dashed", color="black") +
-    geom_boxplot(alpha=0.7) +
-    geom_line(aes(group=paste(sample, drug, IR), linetype=IR), alpha=0.15) +
-    ggbeeswarm::geom_quasirandom(aes(shape=IR), width=0.2, alpha=0.9) +
-    facet_grid(drug ~ cell_line) +
-    ggsignif::geom_signif(y_position=1.45, color="black", tip_length=0,
-        test=function(...) t.test(..., paired=TRUE),
-        map_signif_level=cm$fmt_p, parse=TRUE,
-        comparisons=list(c("Luc", "RBM14"))) +
-    scale_shape_manual(values=c(21,22)) +
-    coord_cartesian(ylim=c(0.5, 1.55)) +
-    scale_fill_brewer(palette="Set1") +
-    labs(x = "Gene overexpressed",
-         y = "Relative viability 1 uM drug vs. DMSO",
-         shape = "IR",
-         fill = "Gene\noverexpressed") +
-    cm$theme_minimal()
+print(resp_curves(dset) + ggtitle("Viability normalized to sample + Luc"))
+print(resp_curves(dset2) + ggtitle("Viability normalized to sample + treatment"))
+print(luc_vs_rbm_1uM(dset2))
+print(resp_curves(dset3) + ggtitle("Viability normalized to sample + treatment + HCC70 0.1uM"))
+print(luc_vs_rbm_1uM(dset3))
 dev.off()
